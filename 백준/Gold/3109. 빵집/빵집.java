7ccc89daed6e20cbecadcda5264eb819471f955e
jihaneol <104291422+jihaneol@users.noreply.github.com>
2023-12-11 19:30:48 +0900

    [Gold II] Title: 빵집, Time: 292 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,58 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    private static char[][] map;
+    private static int ans = 0;
+    private static int N,M;
+    private static   int[] dx = {-1 ,0 ,1};
+    private static  int[] dy = { 1 ,1 ,1};
+    public static void main(String[] args) throws Exception{
 
-	private static int R, C;
-	private static char[][] map; // 맵 상태
-	private static int[][] direct = { { -1, 1 }, { 0, 1 }, { 1, 1 } };
-	private static int answer;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.valueOf(st.nextToken());
+        M = Integer.valueOf(st.nextToken());
+        map = new char[N][];
 
-		// initialize
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		map = new char[R][];
-		for (int i = 0; i < R; i++) {
-			map[i] = br.readLine().toCharArray();
-		}
+        for(int i=0; i<N; i++){
+            map[i] = br.readLine().toCharArray();
+        }
 
-		// solution
-		for (int i = 0; i < R; i++) {
-			if (pipeline(i, 0)) {
-				answer++;
-			}
-		}
+        for(int i=0; i<N; i++){
+            if(connect(i, 0)) ans++;
+        }
 
-		// result
-		System.out.println(answer);
-	}
+        System.out.println(ans);
 
-	public static boolean pipeline(int x, int y) {
-		// 나 여기에 한번 설치 해봤었어
-		map[x][y] = 'o';
-		// 빵집 도착!
-		if (y == C - 1) {
-			return true;
-		}
-		// 각 방향으로 이동해보자
-		for (int d = 0; d < 3; d++) {
-			int dx = x + direct[d][0];
-			int dy = y + direct[d][1];
-			if (inRange(dx, dy) && map[dx][dy] == '.' && pipeline(dx, dy)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    }
 
-	public static boolean inRange(int x, int y) {
-		return 0 <= x && x < R && 0 <= y && y < C;
-	}
+    private static boolean connect(int x, int y) {
+        map[x][y] = 'x';
+
+        if(y == M-1){
+            return true;
+        }
+        for(int i=0; i<3; i++){
+            int nx = dx[i] +x;
+            int ny = dy[i] +y;
+            if(isRange(nx,ny) && map[nx][ny] == '.' && connect(nx,ny)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isRange(int nx, int ny) {
+        if(nx>=0 && ny>=0 && nx<N && ny<M) return true;
+        return false;
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
