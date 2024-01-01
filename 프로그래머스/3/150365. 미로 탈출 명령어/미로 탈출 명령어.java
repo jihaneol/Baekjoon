3fec8642a4155b6206b7ec82365dd2d8e62cd111
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-01-01 23:37:03 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.lang.*;
 class Solution {
-    int[][] array;
-    String answer = null;
     StringBuilder route;
     char[] dir = {'d', 'l', 'r', 'u'};
     int[] rdir = {1, 0, 0, -1};
@@ -11,15 +9,13 @@ class Solution {
     int arrRow, arrCol; //미로 길이
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         route = new StringBuilder();
-        array = new int[n][m];
         endRow = r; endCol = c;
         arrRow = n; arrCol = m;
         //최단거리 계산 - 거리 k로 갈 수 있는지 여부
         int length = distance(x, y, r, c);
         if((k - length) % 2 == 1 || k < length) return "impossible";
-        dfs(x, y, 0, k);
-        
-        return answer == null ? "impossible" : answer;
+  
+        return !dfs(x,y,0,k) ? "impossible" : route.toString();
     }
 
     private int distance(int x, int y, int r, int c){
@@ -30,7 +26,6 @@ class Solution {
         
         if(depth + distance(r, c, endRow, endCol) > k) return false; //현재 깊이 + 남은 거리 > k
         if(k == depth) {
-            answer = route.toString();
             return true;
         }
         for(int i=0; i<4; i++){
@@ -43,7 +38,6 @@ class Solution {
                 }
                 route.delete(depth, depth+1);
             }
-            
         }
         return false;
     }
