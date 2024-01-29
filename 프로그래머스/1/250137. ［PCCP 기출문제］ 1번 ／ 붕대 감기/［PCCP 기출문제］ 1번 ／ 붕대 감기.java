981b2def0295251db09ce6bdb8bc6919c6465e8f
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-01-29 14:09:52 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -6,24 +6,21 @@ public class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
         
         int now = 0; // 현재시간
-        int contiAttack = 0;
+        int cnt = 0; //연속 성공 횟수
         int nowHealth = health;
         for(int[] attack : attacks){
-            for(int i=now+1; i<attack[0]; i++){
-                if(++contiAttack == bandage[0]){
-                    contiAttack = 0;
-                    nowHealth+=bandage[2];
-                }
-                nowHealth+=bandage[1];
-                if(nowHealth>health) nowHealth=health;
-            }
-            contiAttack = 0;
-            now = attack[0];
             
+            int diff = attack[0] - now -1; //시간차이
+            cnt = diff/bandage[0]; // 연속 횟수
+            
+            // 회복
+            nowHealth = Math.min(health,cnt*bandage[2] + nowHealth); //추가
+            nowHealth = Math.min(health,diff*bandage[1] + nowHealth); //초당 회복
+            // 공격 당함
+            now = attack[0];
             nowHealth-=attack[1];
             
             if(nowHealth<=0) return -1;
-            
         }
         
         return nowHealth;
