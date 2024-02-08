53d6981e38ddb6ed79372607d003d75a2e66cb8f
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-02-08 23:45:40 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 42.50 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -5,6 +5,7 @@ class Solution {
     public Map<Integer,Integer> map;
     public int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
     public boolean[][] visited;
+    
     public int solution(int[][] land) {
         int answer = 0;
         key = 2;
@@ -12,16 +13,15 @@ class Solution {
         n = land.length;
         m = land[0].length;
         visited = new boolean[n][m];
+        
         for(int i=0; i<m; i++){
-            
             int oil = getOil(i, land);
-            
             answer = Math.max(answer,oil);
-            
         }
        
         return answer;
     }
+    
     public int getOil(int start, int[][] land){
         boolean[] visitedKey = new boolean[100000];
         int oilTotal = 0;
@@ -39,28 +39,27 @@ class Solution {
                     map.put(key,oilAmount);
                     oilTotal+=oilAmount;
                     visitedKey[key++] = true;
-                     
                 }
-                
             }
         }
         
         return oilTotal;
-        
     }
+    
     public int bfs(int key,int x, int y,int[][] land){
         Deque<int[]> q = new ArrayDeque();
         q.add(new int[] {x,y});
         visited[x][y] = true;
         land[x][y] = key;
         int result = 1;
+        
         while(!q.isEmpty()){
             int[] now = q.poll();
             for(int i=0; i<4; i++){
                 int nx = now[0] + dir[i][0];
                 int ny = now[1] + dir[i][1];
                 
-                if(isRange(nx,ny) || visited[nx][ny] || land[nx][ny]==0 ) continue;
+                if(isRange(nx,ny) || visited[nx][ny] || land[nx][ny]==0) continue;
                 result++;
                 land[nx][ny] = key;
                 q.add(new int[]{nx,ny});
@@ -69,6 +68,7 @@ class Solution {
         }
         return result;
     }
+    
     public boolean isRange(int x, int y){
         return x<0 || y<0 || x>=n || y>=m;
     }
