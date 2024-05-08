3b3e806b87536a947d2819c96159921c5f53a286
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-05-08 14:30:06 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2616 ms, Memory: 329032 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] parents;
+
+    private static class Town implements Comparable<Town> {
+        int s, e, v;
+
+        Town(int s, int e, int v) {
+            this.s = s;
+            this.e = e;
+            this.v = v;
+        }
+
+        public int compareTo(Town t){
+            return Integer.compare(this.v, t.v);
+        }
+
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parents = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            parents[i] = i;
+        }
+
+        Town[] arr = new Town[m];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            arr[i] = new Town(s,e,v);
+        }
+        Arrays.sort(arr);
+        // n-2 까지
+        int cnt = 0;
+        int answer = 0;
+        for(Town town : arr){
+
+            if(cnt == n-2) break;
+            if(union(town.s, town.e)){
+                cnt++;
+                answer += town.v;
+            }
+        }
+        System.out.println(answer);
+
+    }
+
+    public static int find(int x) {
+        if (parents[x] == x) return x;
+
+        return parents[x] = find(parents[x]);
+    }
+
+    public static boolean union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) {
+            return false;
+        } else {
+            parents[x] = y;
+            return true;
+        }
+    }
+
+}
