54972ed56275826038a147eac3b2d1339c45dba4
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-06-01 06:33:15 +0900

    [Gold IV] Title: 스도쿠, Time: 432 ms, Memory: 130576 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,79 +1,85 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+    static int[][] map;
 
-	static int[][] arr =new int[9][9];
-	static boolean flag;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		
-		for(int i=0;i<9;i++) {
-			char[] c = br.readLine().toCharArray();
-			for(int j=0;j<9;j++) {
-				arr[i][j] = c[j]-'0';
-			}
-		}
-		
-		dfs(0);
-		
-		for(int[] a : arr) {
-			for(int b : a) {
-				sb.append(b);
-			}
-			sb.append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
-	private static void dfs(int d) {
-		
-		if(d==81) {
-			flag = true;
-			return;
-		}
-	
-		int r = d/9;
-		int c = d%9;
-		
-		if(arr[r][c]!=0)
-			dfs(d+1);
-		else {
-			for(int i=1;i<10;i++) {
-				if(!isValid(r,c,i))continue;
-				arr[r][c] = i;
-				dfs(d+1);
-				
-				// 종료 조건이 아니라면 더이상 선택할 수가 없다는 뜻 => 백트랙킹
+    public static void main(String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        map = new int[9][9];
 
-				if(flag) return;
-				arr[r][c]=0;
-				
-			}
-		}
-		
-		
-		
-	}
-	private static boolean isValid(int r, int c, int n) {
-		
-		for(int i=0;i<9;i++) {
-			if(arr[i][c]==n || arr[r][i]==n) return false;
-		}
+        for (int i = 0; i < 9; i++) {
+            char[] c = br.readLine().toCharArray();
+            for (int j = 0; j < 9; j++) {
+                map[i][j] = c[j]-'0';
+            }
+        }
+        fillMap(0,0,0,0);
 
-		int sr = r/3 * 3;
-		int sc = c - c%3;
-		for(int row=sr;row<sr+3;row++) {
-			for(int col=sc;col<sc+3;col++) {
-				if(arr[row][col]==n) return false;
-			}
-		}
-		
-		
-		return true;
-		
-	}
+    }
 
+    private static void fillMap(int r, int c, int x, int y) {
+        int now = map[r][c];
+
+        if (now == 0) {
+            boolean visited[] = new boolean[9];
+            int temp;
+            // 행 열 구하기
+            for(int i=0; i<9; i++){
+                temp = map[r][i];
+                if(temp!=0){
+                    visited[temp-1] = true;
+                }
+
+                temp = map[i][c];
+                if(temp!=0){
+                    visited[temp-1] = true;
+                }
+            }
+            // 9자리 확인
+            for(int i=x; i<x+3; i++){
+                for(int j=y; j<y+3; j++){
+                    if(map[i][j]!=0){
+                        visited[map[i][j]-1] = true;
+                    }
+                }
+            }
+            // 후보군 보기
+            for(int i=0; i<9; i++){
+                if(!visited[i]){
+                    map[r][c] = i+1;
+                    fillMap(r,c,x,y);
+                    map[r][c] = 0;
+                }
+            }
+
+        } else {
+            if (c < 8) {
+                if (c % 3 == 2)
+                    fillMap(r, c + 1, x, y + 3);
+                else
+                    fillMap(r, c + 1, x, y);
+            } else {
+                if (r < 8) {
+                    if (r % 3 == 2)
+                        fillMap(r + 1, 0, x + 3, 0);
+                    else
+                        fillMap(r + 1, 0, x, 0);
+                }else{
+                    for(int i=0; i<9; i++){
+                        for(int j=0; j<9; j++){
+                            System.out.print(map[i][j]);
+                        }
+                        System.out.println();
+                    }
+                    System.exit(0);
+                }
+            }
+
+        }
+
+
+    }
 }
