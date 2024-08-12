f5edc6164267cda4234715cf4005a0e16c659c82
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-08-12 16:39:38 +0900

    [level 3] Title: 억억단을 외우자, Time: 463.84 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        int[] answer = new int[starts.length];
+        int[] dp = new int[e+1];
+        Arrays.fill(dp,1);
+        count(dp,e);
+        int idx = starts.length-1;
+        int max = 0;
+        int num = 0;
+        int[] temp = starts.clone();
+        Arrays.sort(temp);
+        Map<Integer,Integer> map = new HashMap();
+        for(int i=e; i>=1; i--){
+            if(dp[i]>=max){
+                max=dp[i];
+                num = i;
+            }
+            if(temp[idx]==i){
+                map.put(i,num);
+                idx--;
+                if(idx==-1) break;
+            }
+            
+        }
+        for(int i=0; i<starts.length; i++){
+            answer[i] = map.get(starts[i]);
+        }
+        return answer;
+    }
+    public void count(int[] dp, int n){
+        for(int i=2; i<=n; i++){
+            for(int j=1; j*i<=n; j++){
+                dp[i*j]++;
+            }
+        }
+    }
+}
