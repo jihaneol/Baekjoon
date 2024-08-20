3b3fd9b0a246739a402e3c6fb2b4dda509ec9c57
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-08-21 00:28:20 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.93 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,11 +1,10 @@
 class Solution {
-    int n, m;
+    int n, m, answer;
     int[][] board;
     int[][] t;
-    int ans = Integer.MAX_VALUE;
+    final int INF = 10000;
     public int solution(int[][] beginning, int[][] target) {
-        int answer = 0;
-        //초기 상태에서 목표 상태로 만들기 위해 필요한 동전 뒤집기 횟수의 최솟값을 return 
+        answer = INF;
         n = beginning.length;
         m = beginning[0].length;
         
@@ -17,17 +16,12 @@ class Solution {
         
         dfs(0, 0);
         
-        if(ans==Integer.MAX_VALUE){
-            answer = -1;
-        } else answer = ans;
-        
-        return answer;
+        return answer == INF ? -1 : answer;
     }
     
     public void flip_row(int r){
-        //0->1, 1->0
         for(int i=0; i<m; i++){
-            board[r][i] = (board[r][i]+1)%2;
+            board[r][i] ^= 1;
         }
     }
     
@@ -45,28 +39,24 @@ class Solution {
     }
     
     public void dfs(int r, int cnt){
-        //모든 행의 경우의 수를 확인했다면
         if(r==n){
-            boolean flag = true;
             for(int i=0; i<m; i++){
                 int result = compare_col(i);
                 if(result==-1){
-                    flag = false;
-                    break;
+                    return;
                 }
                 cnt += result; //전부 반대일 경우 +1
             }
             
-            if(flag){
-                ans = Math.min(ans, cnt);   
-            }
+            answer = Math.min(answer, cnt);   
+            
             return;
         }
         
-        flip_row(r); //행 뒤집기
-        dfs(r+1, cnt+1); //행을 뒤집는 경우
-        flip_row(r); //다시 원상태로
+        flip_row(r); 
+        dfs(r+1, cnt+1); 
+        flip_row(r); 
         
-        dfs(r+1, cnt); //행을 뒤집지 않는 경우
+        dfs(r+1, cnt); 
     }
 }
