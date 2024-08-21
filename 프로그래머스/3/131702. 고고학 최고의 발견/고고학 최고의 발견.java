e90423a4a38eae09ba0354618ad54a7cf353d7ec
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-08-21 19:41:28 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 357.55 ms, Memory: 99.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    public int n, answer, count;
+    public int solution(int[][] clockHands) {
+        n = clockHands.length;
+        answer = Integer.MAX_VALUE;
+        int[][] copyMap = new int[n][n];
+        // 첫번째 칸 해결
+        for(int bit1=0; bit1<Math.pow(4,n); bit1++){
+            deepCopy(copyMap,clockHands);
+            
+            int bit = bit1;
+            int cnt = 0;
+            for(int i=0; i<n; i++){
+                cnt += bit%4;
+                rotationClock(0,i,copyMap,bit%4);
+                bit>>=2;
+            }
+           
+            dfs(n,copyMap, cnt);
+        }
+       
+        return answer;
+    }
+    public void deepCopy(int[][] temp, int[][] clock){
+        for(int i=0; i<temp.length; i++){
+        
+            temp[i] = clock[i].clone();
+        }
+    }
+    public void dfs(int xy, int[][] clockHands, int cnt){
+        count++;
+        if(xy==n*n){
+              
+                  System.out.println();
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(clockHands[i][j]!=0) return;
+                }
+            }
+            answer = Math.min(answer, cnt);
+            return;
+        }
+        int x = xy/n;
+        int y = xy%n;
+    
+        if(clockHands[x-1][y]==0){
+            dfs(xy+1, clockHands, cnt);
+            return;
+        }
+        int num = 4-clockHands[x-1][y];
+        //돌리기
+        rotationClock(x, y, clockHands,num);
+       
+        dfs(xy+1, clockHands, cnt+num);
+ 
+    }
+
+    public void rotationClock(int x, int y,int[][] clockHands, int num){
+        clockHands[x][y] = (clockHands[x][y]+num)%4; 
+        for(int i=0; i<4; i++){
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            if(!isRange(nx,ny)) continue;
+            clockHands[nx][ny] = (clockHands[nx][ny]+num)%4; 
+        }
+    }
+    public boolean isRange(int x, int y){
+        return x>=0 && y>=0 && x<n && y<n;
+    }
+}
