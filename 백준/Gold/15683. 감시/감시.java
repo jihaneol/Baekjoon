84a669d9dc9070ef904d6478f345345014a5f3a3
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-09-15 20:53:48 +0900

    [Gold III] Title: 감시, Time: 136 ms, Memory: 29292 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,135 +1,99 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    private static int[][][] arr = {{{1}, {2}, {3}, {4}},
-            {{1, 3}, {2, 4}},
-            {{1, 2}, {2, 3}, {3, 4}, {1, 4}},
-            {{1, 2, 4}, {1, 2, 3}, {2, 3, 4}, {4, 3, 1}},
-            {{1, 2, 3, 4}}};
-    /**
-     * 초반에 한짓 지우기
-     * private static int[][][] arr2 = {{{1}, {2}, {3}, {4}},
-     * {{1, 3}, {2, 4}},
-     * {{1}, {2}, {3}, {1, 4}},
-     * {{4}, {1}, {2}, {1, 3, 4}},
-     * {{1, 2, 3, 4}}};
-     */
 
-    private static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private static int n, m, ans;
+    private static int[][] map;
+    private static int[][] dir = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+    private static int[][][] cctv =
+            {{}
+                    , {{0}, {1}, {2}, {3}}
+                    , {{0, 2}, {1, 3}}
+                    , {{0, 1}, {1, 2}, {2, 3}, {0, 3}}
+                    , {{0, 1, 2}, {0, 1, 3}, {2, 3, 0}, {1, 2, 3}}
+                    , {{1, 2, 3, 0}}};
 
+    private static List<Pos> aircons = new ArrayList<>();
 
-    private static List<Position> cctv;
+    public static class Pos {
+        int x, y, no;
 
-    private static int total, answer, n, m, blindSpot;
-
-    private static class Position {
-        int x, y;
-
-        Position(int x, int y) {
+        Pos(int x, int y, int no) {
             this.x = x;
             this.y = y;
+            this.no = no;
         }
     }
 
     public static void main(String[] args) throws Exception {
-        // 감시 하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        int[][] map = new int[n][m];
-
-        cctv = new ArrayList<>();
-        total = 0; //
-        blindSpot = 0;
-        answer = 0;
-
+        map = new int[n][m];
+        ans = 65;
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] != 0 && map[i][j] != 6) {
-                    cctv.add(new Position(i, j));
-                } else if (map[i][j] == 0) {
-                    blindSpot++;
-                }
-
+                if (map[i][j] > 0 && map[i][j] < 6) {
+                    aircons.add(new Pos(i, j, map[i][j]));
+                } else if (map[i][j] == 0) cnt++;
             }
         }
 
-        solution(0, map);
+        dfs(0, cnt, aircons.size());
 
-        System.out.println(blindSpot - answer);
-
-
+        System.out.println(ans);
     }
 
-    public static void solution(int depth, int[][] originMap) {
-
-        if (cctv.size() == depth) {
-            if (answer < total) {
-                answer = total;
-            }
+    public static void dfs(int depth, int spot, int max) {
+        if (max == depth) {
+            // 사각지대 수 체크
+            if (ans > spot)
+                ans = spot;
             return;
         }
 
-        Position now = cctv.get(depth);
-        int num = originMap[now.x][now.y];
+        Pos now = aircons.get(depth);
+        Queue<Pos> q = new LinkedList<>();
 
-        for (int i = 0; i < arr[num - 1].length; i++) {
-            int[][] tempMap = copyMap(originMap);
-            int tempTotal = 0;
-            // 그리기
-            for (int j = 0; j < arr[num - 1][i].length; j++) {
-                tempTotal += go(now.x, now.y, arr[num - 1][i][j], tempMap);
-            }
-            total += tempTotal;
-            solution(depth + 1, tempMap);
-            // 지우기
-            total -= tempTotal;
-        }
+        for (int[] d : cctv[now.no]) {
 
-    }
+            int cnt = 0;
+            // 7 추가
+            for (int idx : d) {
+                int dx = dir[idx][0];
+                int dy = dir[idx][1];
 
-    public static int[][] copyMap(int[][] map) {
-        int[][] copy = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                copy[i][j] = map[i][j];
-            }
-        }
-        return copy;
-    }
+                int nx = now.x + dx;
+                int ny = now.y + dy;
+                while (nx >= 0 && ny >= 0 && nx < n && ny < m && map[nx][ny] != 6) {
 
-    // 7이 감시할 수 있는 영역
-    public static int go(int x, int y, int d, int[][] map) {
-        int nx = x;
-        int ny = y;
-        int temp = 0;
-        while (true) {
-            nx += dir[d - 1][0];
-            ny += dir[d - 1][1];
-            if (isRange(nx, ny)) {
-                if (map[nx][ny] == 6) return temp;
-                if (map[nx][ny] == 0) {
-                    map[nx][ny] = 7;
-                    temp++;
+                    if (map[nx][ny] == 0) {
+                        cnt++;
+                        map[nx][ny] = 7;
+                        q.add(new Pos(nx, ny, 7));
+                    }
+
+                    nx += dx;
+                    ny += dy;
+
                 }
-            } else {
-                return temp;
+            }
+            dfs(depth + 1, spot - cnt, max);
+            // 7 삭제
+            while (!q.isEmpty()) {
+                Pos del = q.poll();
+
+                map[del.x][del.y] = 0;
             }
         }
+
+
     }
-
-
-    public static boolean isRange(int x, int y) {
-        return x >= 0 && y >= 0 && x < n && y < m;
-    }
-
 }
