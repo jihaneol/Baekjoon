3236367655eb2d8a289440406d97ac6fc94e34ea
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-09-20 20:32:05 +0900

    [level 3] Title: n + 1 카드게임, Time: 11.07 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -2,65 +2,73 @@ import java.util.*;
 
 class Solution {
     Set<Integer> original, additional;
+    int n;
     public int solution(int coin, int[] cards) {
         original = new HashSet();
         additional = new HashSet();
-        int n = cards.length;
+        n = cards.length;
         for(int i=0; i<n/3; i++){
             original.add(cards[i]);
         }
-        int round = 0;
         
-        int x = n/3;
+        
+        int x = n/3; // 뽑은 카드 수
         int target = n+1;
-        while(true){
-            boolean isEnd = false;
-            round++;
-            if(x>=n) break;
-            int A = cards[x];
-            int B = cards[x+1];
-            additional.add(A);
-            additional.add(B);
-            x+=2;
-            // 내가 갖고 있는 것 비교
-            for(int card  : original){
-                if(original.contains(target-card)){
-                    original.remove(card);
-                    original.remove(target-card);
-                    isEnd = true;
-                    break;
-                }
-            }
-            // 1장 뽑아서 비교
-            if(!isEnd && coin>0){
-                for(int card : additional){
-                    if(original.contains(target-card)){
-                        original.remove(target-card);
-                        additional.remove(card);
-                        --coin;
-                        isEnd = true;
-                        break;
-                    }
-                }
-            }
-            
-            // 2장 뽑아서 비교
-            if(!isEnd && coin>1){
-                for(int card : additional){
-                    if(additional.contains(target-card)){
-                        additional.remove(target-card);
-                        additional.remove(card);
-                        coin-=2;
-                        isEnd = true;
-                        break;
-                    }
-                }
-            }
-            // 없으면 나가기
-            if(!isEnd){
+        return play(1,x, coin, cards);
+    
+    }
+    public int play(int round,int idx ,int coin, int[]cards){
+        
+        if(idx==n){
+            return round;
+        }
+        
+        int a = cards[idx];
+        int b = cards[idx+1];
+        additional.add(a);
+        additional.add(b);
+        boolean isSolve = false;
+        
+        //기존으로 해결 가능한가?
+        for(int card : original){
+            if(original.contains(n-card+1)){
+                original.remove(n-card+1);
+                original.remove(card);
+                isSolve = true;
                 break;
             }
         }
+        
+        // 코인 하나 써야 한다. 오리지날과 추가한 부분 확인
+        if(!isSolve && coin>0){
+            for(int card : original){
+                if(additional.contains(n-card+1)){
+                    original.remove(card);
+                    additional.remove(n-card+1);
+                    coin--;
+                    isSolve = true;
+                    break;
+                }
+            }
+        }
+        
+        // 코인 두개 쓰고. 추가한 부분들만 보자
+        if(!isSolve && coin>1){
+            for(int card : additional){
+                if(additional.contains(n-card+1)){
+                    additional.remove(card);
+                    additional.remove(n-card+1);
+                    coin-=2;
+                    isSolve = true;
+                    break;
+                }
+            }
+        }
+        
+        if(isSolve) {
+            return play(round+1,idx+2,coin, cards);
+        }
+        
         return round;
         
     }
