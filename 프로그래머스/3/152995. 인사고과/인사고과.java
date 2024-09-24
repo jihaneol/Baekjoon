40b5372cb0a834f49f9c990f2c31bdc81f64e666
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-09-24 14:38:16 +0900

    [level 3] Title: 인사고과, Time: 166.07 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,22 +1,24 @@
 import java.util.*;
 
+// 근무 태도, 동료 평가 - 모두 낮으면 인센 못받아. 합으로 석차
+
 class Solution {
     public int solution(int[][] scores) {
         int[] s = scores[0];
         Arrays.sort(scores, (s1, s2) -> s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0]);
   
         int max = 0, ans = 1;
-        for(int[] score : scores) {
-          
-            if(score[1] < max) {
-                if(score.equals(s))
-                    return -1;
+        for(int[] score : scores) {   
+            // 둘다 작다.
+            if(score[1] < max && score.equals(s)) {
+                return -1;
             }
-            else {
-                max = Math.max(max, score[1]);
-                if(s[0] + s[1] < score[0] + score[1])
-                    ans++;
-            }
+            if(score[1] < max) continue;
+            
+            // 비교 대상
+            max = Math.max(max, score[1]);
+            if(s[0] + s[1] < score[0] + score[1])
+                ans++;
         }
         return ans;
     }
