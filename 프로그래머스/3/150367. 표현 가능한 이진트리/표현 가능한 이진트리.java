434a7719f9b266c82b36840554a64651cfa65f2e
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-10-03 23:38:46 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 60.77 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class Solution {
-    
+
     public List solution(long[] numbers) {
         List<Integer> answer = new ArrayList();
-        
+
         for(long number : numbers){
             String s = Long.toBinaryString(number);
             int i = 0;
@@ -17,27 +17,28 @@ class Solution {
         }
         return answer;
     }
-    
-    
+
+
     private boolean check(String number){
         boolean valid = true;
-		
-		int mid = (number.length()-1)/2;
-		char root = number.charAt(mid);
-		String left = number.substring(0,mid);
-		String right = number.substring(mid+1,number.length());
-		
-		if(root == '0' && (left.charAt((left.length()-1)/2)=='1' || right.charAt((right.length()-1)/2)=='1')){
-			return false;
-		}
-		
-		if(left.length() >= 3) {
-			valid = check(left);
-			if(valid) {
-				valid = check(right);
-			}
-		}
-		return valid;
-        
+
+        int mid = (number.length()-1)/2;
+        char root = number.charAt(mid);
+        String left = number.substring(0,mid);
+        String right = number.substring(mid+1,number.length());
+
+        if(root == '0' && (left.charAt((left.length()-1)/2)=='1' 
+                           || right.charAt((right.length()-1)/2)=='1')){
+            return false;
+        }
+
+        if(left.length() >= 3) {
+            valid = check(left);
+            if(valid) {
+                valid = check(right);
+            }
+        }
+        return valid;
+
     }
 }
