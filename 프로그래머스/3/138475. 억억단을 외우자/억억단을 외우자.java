c7d889259e7e459a850d2074e165832e0009e2a4
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-10-07 20:53:23 +0900

    [level 3] Title: 억억단을 외우자, Time: 417.18 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -4,22 +4,24 @@ class Solution {
         int[] answer = new int[starts.length];
         int[] dp = new int[e+1];
         Arrays.fill(dp,1);
-        count(dp,e);
-        int idx = starts.length-1;
-        int max = 0;
-        int num = 0;
-        int[] temp = starts.clone();
-        Arrays.sort(temp);
+        count(dp,e);     
         Map<Integer,Integer> map = new HashMap();
-        for(int i=e; i>=temp[0]; i--){
-            if(dp[i]>=max){
-                max=dp[i];
-                num = i;
+        Integer[] tmp = Arrays.
+            stream(starts.clone()).boxed().toArray(Integer[] :: new);
+        
+        Arrays.sort(tmp,Collections.reverseOrder());
+        int start = e;
+        int max = 0;
+        int idx = 0;
+        for(int end : tmp){
+            for(int i=start; i>=end; i--){
+                if(max<=dp[i]) {
+                    max = dp[i];
+                    idx = i;
+                }
             }
-            if(temp[idx]==i){
-                map.put(i,num);
-                idx--;
-            }
+            map.put(end,idx);
+            start = end;
         }
         for(int i=0; i<starts.length; i++){
             answer[i] = map.get(starts[i]);
@@ -28,7 +30,8 @@ class Solution {
     }
     public void count(int[] dp, int n){
         for(int i=2; i<=n; i++){
-            for(int j=1; j*i<=n; j++){
+            dp[i]++;
+            for(int j=2; j*i<=n; j++){
                 dp[i*j]++;
             }
         }
