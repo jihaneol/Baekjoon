899462108f60867e37e18f57097599120d752e37
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-10-24 09:06:57 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 22.82 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,67 +1,47 @@
 import java.util.*;
- 
+
 class Solution {
     public int solution(int alp, int cop, int[][] problems) {
-            
+
          int goal_a=0;
          int goal_c=0;
         //목표치를 구하는 for문
-        for(int i =0; i<problems.length;i++){
-            goal_a=Math.max(problems[i][0],goal_a);
-            goal_c=Math.max(problems[i][1],goal_c);
+       for(int[] p : problems){
+            goal_a=Math.max(p[0],goal_a);
+            goal_c=Math.max(p[1],goal_c);
         }
+
+        if(goal_a<=alp && goal_c<=cop) return 0;
         
-        if(goal_a<=alp&&goal_c<=cop){
-            return 0;
-        }
-        
-        if(alp>=goal_a){
-            alp=goal_a;
-        }
-        if(cop>=goal_c){
-            cop=goal_c;
-        }
-        
-        int[][] dp =new int[goal_a+2][goal_c+2];
-        
+        alp = Math.min(alp, goal_a);
+        cop = Math.min(cop, goal_c);
+
+        int[][] dp =new int[goal_a+1][goal_c+1];
+
         for(int i=alp;i<=goal_a;i++){
             for(int j=cop;j<=goal_c;j++){
                 dp[i][j]=Integer.MAX_VALUE;
-          }
+            }
         }
-        
+
         dp[alp][cop]=0;
-        
+
          for(int i=alp;i<=goal_a;i++){
             for(int j=cop;j<=goal_c;j++){
+                if(i<goal_a) dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j]+1);
+                if(j<goal_c) dp[i][j+1] = Math.min(dp[i][j+1], dp[i][j]+1);
                 
-                dp[i+1][j]=Math.min(dp[i+1][j],dp[i][j]+1);
-                
-                dp[i][j+1]=Math.min(dp[i][j+1],dp[i][j]+1);
-                
-                for(int[] p :problems){
-    
-                if(i>=p[0]&&j>=p[1]){
-                    if(i+p[2]>goal_a&&j+p[3]>goal_c){
-                      dp[goal_a][goal_c]=Math.min(dp[goal_a][goal_c],dp[i][j]+p[4]);
-                    }
-                    else if(i+p[2]>goal_a){
-                        dp[goal_a][j+p[3]]=Math.min(dp[goal_a][j+p[3]],dp[i][j]+p[4]);
-                    }
-                    else if(j+p[3]>goal_c){
-                        dp[i+p[2]][goal_c]=Math.min(dp[i+p[2]][goal_c],dp[i][j]+p[4]);
-                    }
-                    else if(i+p[2]<=goal_a&&j+p[3]<=goal_c){
-                       dp[i+p[2]][j+p[3]]=Math.min(dp[i+p[2]][j+p[3]],dp[i][j]+p[4]); 
-                    }
+                for(int[] p : problems){
+                    if(i<p[0] || j<p[1]) continue;
+                    int next_a = Math.min(goal_a, p[2]+i);
+                    int next_c = Math.min(goal_c, p[3]+j);
+                    dp[next_a][next_c] = Math.min(dp[next_a][next_c]
+                                                 ,dp[i][j]+p[4]);
                 }
-                                  
-                 }
-                }
-          }
-        
+            }
+         }
         return dp[goal_a][goal_c];
     }
-    
- 
+
+
 }
