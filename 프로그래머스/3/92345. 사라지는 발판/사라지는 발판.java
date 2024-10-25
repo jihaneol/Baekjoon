3d5a2ada5e15f6e18b8793363e9a810b7160199d
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-10-25 14:20:41 +0900

    [level 3] Title: 사라지는 발판, Time: 13.00 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,60 +1,58 @@
 class Solution {
-    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-    private int answer = Integer.MAX_VALUE;
-    private int n,m;
+    
     private class Player{
         boolean isWin;
-        int cnt;
-        Player(boolean w, int c){
-            isWin = w;
-            cnt = c;
+        int num;
+        Player(boolean w, int n){
+            this.isWin = w;
+            this.num = n;
         }
     }
+    private int n,m;
+    private int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
     public int solution(int[][] board, int[] aloc, int[] bloc) {
         n = board.length;
         m = board[0].length;
-        return play(board,aloc[0], aloc[1], bloc[0], bloc[1],0).cnt;
+        return play(board, aloc[0], aloc[1], bloc[0], bloc[1], 0).num;
     }
-    public Player play(int[][] board, int ax, int ay, int bx, int by,int cnt){
     
-       if(isEnd(board,ax,ay) || board[ax][ay]==0){
-           return new Player(false, cnt);
-       }
-     
-        int loseMax = -1; // 지면 최대한 길게
-        int winMin = Integer.MAX_VALUE; // 이기면 최대한 짧게
+    private Player play(int[][] board
+                        , int ax, int ay, int bx, int by,int cnt){
+        
+        if(board[ax][ay]==0){
+            return new Player(false, 0);
+        }
         board[ax][ay] = 0;
+        int winner = Integer.MAX_VALUE;
+        int loser = 0;
+        boolean isEnd = true;
+      
         for(int i=0; i<4; i++){
-            int nx = ax + dir[i][0];
-            int ny = ay + dir[i][1];
-            if(!isRange(nx,ny) || board[nx][ny]==0) continue;
-            Player p = play(board, bx, by, nx, ny, cnt+1);
-            if(p.isWin){
-                loseMax = Math.max(p.cnt, loseMax);
+            int nx = dir[i][0] + ax;
+            int ny = dir[i][1] + ay;
+            if(!isRange(nx, ny) || board[nx][ny]==0) continue;
+            isEnd = false;
+            Player next = play(board, bx, by, nx, ny, cnt+1);
+            // 난 졌다.
+            if(next.isWin){
+                loser = Math.max(loser, next.num);
             }else{
-                winMin = Math.min(p.cnt, winMin);
+                winner = Math.min(winner, next.num);
             }
-          
         }
-          board[ax][ay] = 1;
-
-        if(winMin!=Integer.MAX_VALUE){
-            return new Player(true, winMin);
-            
+        board[ax][ay] = 1;
+        
+        if(isEnd){
+            return new Player(false, loser);
         }
-        return new Player(false, loseMax);
+        
+        return winner==Integer.MAX_VALUE ? 
+            new Player(false,loser+1) : new Player(true, winner+1);
         
     }
-    public boolean isEnd(int[][] board, int x, int y){
-        for(int i=0; i<4; i++){
-            int nx = x + dir[i][0];
-            int ny = y + dir[i][1];
-            if(!isRange(nx,ny) || board[nx][ny]==0) continue;
-            return false;
-        }
-        return true;
+    private boolean isRange(int x, int y){
+        return x>=0 && x<n && y>=0 && y<m;
     }
-    public boolean isRange(int x, int y){
-        return x>=0 && y>=0 && x<n && y<m;
-    }
+   
+    
 }
