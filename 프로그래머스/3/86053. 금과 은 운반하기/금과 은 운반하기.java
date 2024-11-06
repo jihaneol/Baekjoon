adbecc84b856b3918e79869c7d81b885b8fd859e
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-11-06 10:13:36 +0900

    [level 3] Title: 금과 은 운반하기, Time: 145.79 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,34 +1,36 @@
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w1, int[] t1) {
-        long answer = -1;
-        int len = s.length;
-        long left = 1;
-        long right = (long)(10e5*2*10e9*2);
-        while(left<=right){
-            long mid = (left+right)/2; // 시간
-            long silver = 0, gold = 0, sum = 0;
+        
+        long answer = 1;
+        int len = w1.length;
+        long end = (long)10e14*2*2;
+        long start = 1;
+        
+        while(start<=end){
+            long gold = 0, silver = 0, sum = 0;
+            long mid = (start+end)/2;
+            
             for(int i=0; i<len; i++){
                 int w = w1[i];
                 int t = t1[i];
                 
                 long cnt = mid/(t*2);
-                if(mid%(t*2)>=t){
-                    cnt++;
-                }
-                silver += Math.min(s[i],cnt*w);
-                gold += Math.min(g[i],cnt*w);
-                sum += Math.min(g[i]+s[i],cnt*w);
-            }
-            System.out.println(silver+" "+gold+" "+sum+" "+mid);
-        
-            if(silver>=b && gold>=a && sum>=a+b){
-                answer = mid;
-                right = mid-1;
-            }else{
-                left = mid+1;
+                
+                if(mid%(t*2) >=t) cnt++;
+                
+                gold += Math.min(g[i], cnt*w); // 금만
+                silver += Math.min(s[i], cnt*w); // 은만
+                sum += Math.min(g[i] + s[i], cnt*w); // 금, 은 함께
             }
             
+            if(gold>=a && silver>=b && sum>=a+b){
+                answer = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
         }
+        
         return answer;
     }
 }
