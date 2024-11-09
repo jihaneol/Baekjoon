10e7ee2163d5f4eda3af415a147393b94b93a8de
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-11-09 23:58:47 +0900

    [level 3] Title: 표 편집, Time: 772.85 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,29 +1,30 @@
 import java.util.*;
- 
+
 class Solution {
-    
+
     public String solution(int n, int k, String[] cmd) { 
         Stack<Integer> remove = new Stack<>();
         int table_size = n;
-        
+
         for(int i = 0; i < cmd.length; i++) {
             char c = cmd[i].charAt(0);
- 
-            if(c == 'U') {
-                k -= Integer.valueOf(cmd[i].substring(2));
-            } else if(c == 'D') {
-                k += Integer.valueOf(cmd[i].substring(2));
-            } else if(c == 'C') {
-                remove.push(k);
-                table_size -= 1;
-                if(k == table_size) k -= 1;
-            } else {
-                int r = remove.pop(); 
-                if(k >= r) k += 1;
-                table_size += 1;
+            switch(c) {
+                case 'U' : k -= Integer.parseInt(cmd[i].substring(2));
+                    break;
+                case 'D' : k += Integer.parseInt(cmd[i].substring(2));
+                    break;
+                case 'C' :
+                    remove.push(k);
+                    table_size -= 1;
+                    if(k == table_size) k -= 1;
+                    break;
+                default : 
+                    int r = remove.pop(); 
+                    if(k >= r) k += 1;
+                    table_size += 1;
             }
         }
-        
+
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < table_size; i++) {
             sb.append('O');
