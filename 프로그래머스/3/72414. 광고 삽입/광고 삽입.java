03a2517d5f6a7f4ac70706d781b2d5c93e55c421
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-11-20 09:35:31 +0900

    [level 3] Title: 광고 삽입, Time: 309.76 ms, Memory: 302 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,52 +1,83 @@
-import java.util.Arrays;
 
 class Solution {
-
     public String solution(String play_time, String adv_time, String[] logs) {
-        int playTime = toIntTime(play_time);
-        int advTime = toIntTime(adv_time);
-        int[] times = new int[360_000];
-        for (String log : logs) {
-            String[] splitLog = log.split("-");
-            int startTime = toIntTime(splitLog[0]);
-            int endTime = toIntTime(splitLog[1]);
-            for (int i = startTime; i < endTime; i++) {
-                times[i]++;
+        int play = convertTime(play_time);
+        int e = convertTime(adv_time);
+        int s = 0;
+        
+        int[] playTimeArr = new int[play+2];
+        
+        for(String log : logs){
+            String[] temp = log.split("-");
+            int s_log = convertTime(temp[0]);
+            int e_log = convertTime(temp[1]);
+            
+            playTimeArr[s_log]++;
+            playTimeArr[e_log]--;
+        }
+        
+        for(int i=1; i<play+1; i++){
+            playTimeArr[i] +=playTimeArr[i-1];
+        }
+        long total = 0;
+        int adv_start_anser = 0;
+        //초기
+        for(int i=0; i<=e; i++){
+            total+=playTimeArr[i];
+        }
+        long max = total;
+        
+        // 슬라이딩
+        for(int i=e; i<play; i++){
+            total-=playTimeArr[i-e];
+            total+=playTimeArr[i];
+            
+            if(max<total){
+                max = total;
+                adv_start_anser = i-e+1;
             }
         }
 
-        int maxIdx = 0;
-        long totalTime = 0;
-        for (int i = 0; i <= advTime; i++) {
-            totalTime += times[i];
-        }
-        long maxTotalTime = totalTime;
-        for (int i = advTime; i < playTime; i++) {
-            totalTime += times[i] - times[i - advTime];
-            if (totalTime > maxTotalTime) {
-                maxTotalTime = totalTime;
-                maxIdx = i - advTime + 1;
-            }
-        }
-
-        return toStrTime(maxIdx);
+        return convertString(adv_start_anser);
     }
-
-    int toIntTime(String strTime) {
-        int[] HMS = Arrays.stream(strTime.split(":"))
-            .mapToInt(Integer::parseInt)
-            .toArray();
-
-        return 3600 * HMS[0] + 60 * HMS[1] + HMS[2];
+    private int convertTime(String now){
+        String[] time = now.split(":");
+        int hour = Integer.parseInt(time[0])*3600;
+        int minute = Integer.parseInt(time[1])*60;
+        int sec = Integer.parseInt(time[2]);
+        return hour + minute + sec;
     }
-
-    String toStrTime(int time) {
-        int H = time / 3600;
-        int M = (time - 3600 * H) / 60;
-        int S = time - 3600 * H - 60 * M;
-
-        return (H < 10 ? "0" : "") + H + ":" +
-            (M < 10 ? "0" : "") + M + ":" +
-            (S < 10 ? "0" : "") + S;
+    
+    private String convertString(int time){
+        StringBuilder sb = new StringBuilder();
+        int hour = time/3600;
+        time%=3600;
+        int minute = time/60;
+        time%=60;
+        int sec = time;
+        if(check(hour)){
+            sb.append(hour);
+        }else{
+            sb.append(0).append(hour);
+        }
+        sb.append(":");
+        
+         if(check(minute)){
+            sb.append(minute);
+        }else{
+            sb.append(0).append(minute);
+        }
+          sb.append(":");
+        
+         if(check(sec)){
+            sb.append(sec);
+        }else{
+            sb.append(0).append(sec);
+        }
+        return sb.toString();
+    }
+    private boolean check(int time){
+        if(time<10) return false;
+        return true;
     }
 }
