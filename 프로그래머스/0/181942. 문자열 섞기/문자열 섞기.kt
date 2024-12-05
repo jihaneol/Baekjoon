460c2bcef8144f6f99d6cc9b59409ff88f990243
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-12-05 21:41:21 +0900

    [level 0] Title: 문자열 섞기, Time: 15.18 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(str1: String, str2: String): String {
+        var answer: String = ""
+        
+        for(i in 0..str1.length-1){
+            answer +=str1[i]
+            answer +=str2[i]
+        }
+        return answer
+    }
+}
