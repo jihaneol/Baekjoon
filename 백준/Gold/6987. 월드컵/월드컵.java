500f283038f90807a6f9067a7dc9c2e335581b45
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-04-25 21:06:31 +0900

    [Gold IV] Title: 월드컵, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -1,38 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[][] scoreBoard;
-    static int[][] nowScoreBoard;
 
-    static int[][] players = {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6},
-            {2, 3}, {2, 4}, {2, 5}, {2, 6},
-            {3, 4}, {3, 5}, {3, 6},
-            {4, 5}, {4, 6},
-            {5, 6}};
-    static int answer;
+    private static int[][] teams = new int[6][3]; // 6팀 승 무 패
+    private static int[][] rounds = {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5},
+            {1, 2}, {1, 3}, {1, 4}, {1, 5},
+            {2, 3}, {2, 4}, {2, 5},
+            {3, 4}, {3, 5},
+            {4, 5}};
+
+    private static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
-        int game = 4;
-        while (game-- > 0) {
-            answer = 0;
-            scoreBoard = new int[7][3];
-            nowScoreBoard = new int[7][3];
-            st = new StringTokenizer(br.readLine());
+        for (int z = 0; z < 4; z++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             // 값 초기화
-            for (int i = 1; i < 7; i++) {
+            for (int i = 0; i < 6; i++) {
                 for (int j = 0; j < 3; j++) {
-                    scoreBoard[i][j] = Integer.valueOf(st.nextToken());
+                    teams[i][j] = Integer.valueOf(st.nextToken());
                 }
             }
-
-            // 경기 시작
+            answer = 0;
             play(0);
             sb.append(answer).append(" ");
         }
@@ -40,52 +32,53 @@ public class Main {
 
     }
 
-    private static void play(int cnt) {
-
-        if(answer==1) return;
-        if (cnt == 15 ) {
-            if(!isPossible()) return;
+    private static void play(int round) {
+        if (answer == 1) return;
+        if (round == rounds.length) {
+            for (int i = 0; i < teams.length; i++) {
+                for (int j = 0; j < teams[i].length; j++) {
+                    if (teams[i][j] != 0) {
+                        return;
+                    }
+                }
+            }
             answer = 1;
             return;
         }
-        int[] player = players[cnt];
-        int A = player[0];
-        int B = player[1];
+
+        int a = rounds[round][0];
+        int b = rounds[round][1];
         // 승
-        if (scoreBoard[A][0] >= nowScoreBoard[A][0]+1 && scoreBoard[B][2] >= nowScoreBoard[B][2]+1) {
-            nowScoreBoard[A][0]++;
-            nowScoreBoard[B][2]++;
-            play(cnt + 1);
-            nowScoreBoard[A][0]--;
-            nowScoreBoard[B][2]--;
+        if (teams[a][0] - 1 >= 0 && teams[b][2] - 1 >= 0) {
+            teams[a][0]--;
+            teams[b][2]--;
+            // 다음
+            play(round + 1);
+            teams[a][0]++;
+            teams[b][2]++;
+
         }
+
         // 무
-        if (scoreBoard[A][1] >= nowScoreBoard[A][1]+1 && scoreBoard[B][1] >= nowScoreBoard[B][1]+1) {
-            nowScoreBoard[A][1]++;
-            nowScoreBoard[B][1]++;
-            play(cnt + 1);
-            nowScoreBoard[A][1]--;
-            nowScoreBoard[B][1]--;
+        if (teams[a][1] - 1 >= 0 && teams[b][1] - 1 >= 0) {
+            teams[a][1]--;
+            teams[b][1]--;
+            // 다음
+            play(round + 1);
+            teams[a][1]++;
+            teams[b][1]++;
+
         }
         // 패
-        if (scoreBoard[A][2] >= nowScoreBoard[A][2]+1 && scoreBoard[B][0] >= nowScoreBoard[B][0]+1) {
-            nowScoreBoard[A][2]++;
-            nowScoreBoard[B][0]++;
-            play(cnt + 1);
-            nowScoreBoard[A][2]--;
-            nowScoreBoard[B][0]--;
-        }
+        if (teams[a][2] - 1 >= 0 && teams[b][0] - 1 >= 0) {
+            teams[a][2]--;
+            teams[b][0]--;
+            // 다음
+            play(round + 1);
+            teams[a][2]++;
+            teams[b][0]++;
 
+        }
     }
 
-    private static boolean isPossible() {
-        for(int i=1; i<7; i++){
-            int cnt = 0;
-            for(int j=0; j<3; j++){
-                cnt +=scoreBoard[i][j];
-            }
-            if(cnt!=5) return false;
-        }
-        return true;
-    }
 }
