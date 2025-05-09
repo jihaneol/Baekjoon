b739639c3fbcee51a9d04999a8854f5ea9613760
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-05-10 00:01:44 +0900

    [Silver II] Title: 과일 탕후루, Time: 216 ms, Memory: 29408 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,35 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());  
-        int[] fruits = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            fruits[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] type = new int[10];
+        int count = 0; // 종류 개수
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
         }
+        int answer = 0;
 
-        HashMap<Integer, Integer> fruitsCnt = new HashMap<>();
-        int maxLength = 0; 
-        int left = 0; 
-
-        for (int right = 0; right < N; right++) {
-            fruitsCnt.put(fruits[right], fruitsCnt.getOrDefault(fruits[right], 0) + 1);
-
-            while (fruitsCnt.size() > 2) {
-                fruitsCnt.put(fruits[left], fruitsCnt.get(fruits[left]) - 1);
-
-                if (fruitsCnt.get(fruits[left]) == 0) {
-                    fruitsCnt.remove(fruits[left]);
+        int s = 0;
+        int e = 0;
+        while (e < n) {
+            // e 움직이기
+            while (e < n) {
+                int num = arr[e];
+                if (type[num] == 0) {
+                    count++;
+                    if (count == 3) {
+                        type[num]++;
+                        break;
+                    }
                 }
-
-                left++;
+                type[num]++;
+                e++;
             }
-            maxLength = Math.max(maxLength, right - left + 1);
+            answer = Math.max(answer, e - s);
+            // s 움직이기
+            while (s < e && count > 2) {
+                int num = arr[s];
+                if (type[num] == 1) {
+                    count--;
+                }
+                type[num]--;
+                s++;
+            }
+            e++;
         }
-        System.out.println(maxLength);
+        System.out.println(answer);
+
     }
 }
