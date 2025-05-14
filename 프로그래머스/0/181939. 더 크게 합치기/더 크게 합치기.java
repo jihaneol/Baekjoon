bf02e2ca1409c6502d1b6aee7d6de726fff78839
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-05-14 23:13:51 +0900

    [level 0] Title: 더 크게 합치기, Time: 11.42 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int a1 = Integer.parseInt(a+""+b);
+        int b1 = Integer.parseInt(b+""+a);
+        return a1>b1?a1:b1;
+    }
+}
