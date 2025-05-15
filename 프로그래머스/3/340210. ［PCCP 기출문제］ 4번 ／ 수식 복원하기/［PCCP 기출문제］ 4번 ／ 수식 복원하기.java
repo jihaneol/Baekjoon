de46573bcfa302891aa2b6d7b10c670b28daeba6
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-05-15 23:48:45 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 15.67 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -1,69 +1,68 @@
 import java.util.*;
 class Solution {
-    private  Queue<Integer> bits = new LinkedList();
-    private final String ERROR = "ERROR";
-    public List<String> solution(String[] expressions) {
-        List<String> answer = new ArrayList();
-        List<String> xList = new ArrayList();
-        for(int i=2; i<10; i++){
-            bits.add(i);
-        }
-        for(String exp : expressions){
-            String[] e = exp.split(" "); // 0 2 4
-            if("X".equals(e[4])){
-                xList.add(exp);
-            }
-            cal(e);
-        }
-        
-        
-        Set<String> set = new HashSet();
-        String tmp = "";
-        for(String exp : xList){
-            String[] e = exp.split(" ");
-            for(int bit : bits){
-                tmp = getResult(bit,e);
-                if(!tmp.equals(ERROR)){
-                    set.add(tmp);
-                }
-            }
     
-            exp = exp.substring(0,exp.length()-1);
-            if(set.size()==1){
-                answer.add(exp+tmp);
-            }else{
-                answer.add(exp+"?");
+    public String[] solution(String[] expressions) {
+        List<Integer> radixList = new ArrayList();
+        for(int base = 2; base<10; base++){
+            if(validate(expressions, base)){
+                radixList.add(base);
             }
-            set.clear();     
+        }
+        List<String> answer = new ArrayList();
+        for(String expression : expressions){
+            String[] split = expression.split("=");
+            
+            if("X".equals(split[1].trim())){
+                
+                Set<String> radixSet = new HashSet();
+                String[] exp = split[0].split(" ");
+                String res = "";
+                for(int base : radixList){
+                    res = Integer.toString(calcu(exp, base),base);
+                    radixSet.add(res);
+                  
+                }
+       
+                if(radixSet.size()==1){
+                    answer.add(split[0]+"= "+res);
+                }else{
+                    answer.add(split[0]+"= ?");
+                }
+                
+            }
         }
         
-        return answer;
+        return answer.stream().toArray(String[]::new);
     }
-    public void cal(String[] e){
-        int size= bits.size();
-        while(size-->0){
-            int bit = bits.poll();
-            String b =  getResult(bit,e);
-            if(b.equals(ERROR)) continue;
-            if(b.equals(e[4]) || "X".equals(e[4])){
-                 bits.add(bit);
+    private boolean validate(String[] expressions, int base){
+        
+        for(String expression : expressions){
+            
+            for(int i=0; i<expression.length(); i++){
+                char c = expression.charAt(i);
+                if(Character.isDigit(c) && base <= c -'0') return false;
+            }
+            
+            String[] split = expression.split("=");
+            String res = split[1].trim();
+            
+            if(!"X".equals(res)){
+                int target = calcu(split[0].split(" "), base);
+                if(Integer.parseInt(res,base) != target) return false;
             }
         }
+        return true;
     }
-    public String getResult(int bit,String[] e){
-  
-        try{
-             int a;
-             if("+".equals(e[1])){
-                 a=Integer.parseInt(e[0],bit)+Integer.parseInt(e[2],bit);
-             }else{
-                 a=Integer.parseInt(e[0],bit)-Integer.parseInt(e[2],bit);
-             }
-             return Integer.toString(a,bit);
-        }catch(Exception e1){
-            return ERROR;
+    private int calcu(String[] exp, int base){
+        String a = exp[0];
+        String op = exp[1];
+        String b = exp[2];
+        
+        if("+".equals(op)){
+            return Integer.parseInt(a, base) + Integer.parseInt(b,base);
+        }else{
+            return Integer.parseInt(a, base) - Integer.parseInt(b,base);
         }
-     
     }
-     
+    
 }
