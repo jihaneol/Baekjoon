ac30edefb22f288509cbebf2d41a15ae8d92ff81
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-05-21 18:38:52 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 224.09 ms, Memory: 226 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -1,28 +1,27 @@
 class Solution {
-    private  class Position {
+    private static class Position {
         int x,y;
         Position(int x, int y){
             this.x = x;
             this.y = y;
         }
-        public Position next(int dir){
-            return new Position(x+dx[dir], y+dy[dir]);
-        }
     }
-    private int[][] map;
-    private  int[] dx = {-1,1,0,0};
-    private  int[] dy = {0,0,-1,1};
-    private  boolean visited[][][], redEnd, blueEnd;
-    private int answer = 20;
+    private static int[][] map;
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+    private static boolean visited[][][], redEnd, blueEnd;
+
 
     public int solution(int[][] maze) {
-        this.map = maze;
+        map = new int[maze.length][maze[0].length];
+        int answer = 0;
         Position blue_position = null;
         Position red_position = null;
         visited = new boolean[maze.length][maze[0].length][2];// 0 레드, 1 브루
 
         for(int i=0; i<maze.length; i++){
             for(int j=0; j<maze[i].length; j++){
+                map[i][j] = maze[i][j];
                 switch(maze[i][j]){
                     case 1: 
                         red_position = new Position(i,j);
@@ -36,26 +35,25 @@ class Solution {
             }
         }
 
-       backtracking(red_position,blue_position,0);
+        answer = backtracking(red_position,blue_position,0);
 
         return answer==20?0:answer;
     }
-    private void backtracking(Position red_position, Position blue_position, int result){
+    private static int backtracking(Position red_position, Position blue_position, int result){
 
         if(redEnd && blueEnd) {
-            answer = Math.min(answer, result);
-            return;
-            
+        
+            return result;
         }
-     
+        int min = 20;
 
         // 수레 움직이기 완탐.
         for(int i=0; i<4; i++){
-            Position nextRed = !redEnd? red_position.next(i):red_position;
+            Position nextRed = !redEnd? nextPosition(red_position,i):red_position;
+            if(!isRange(nextRed.x, nextRed.y))continue;
             for(int j=0; j<4; j++){
-                Position nextBlue = !blueEnd ? blue_position.next(j):blue_position;
-
-                if(!isPossible(nextRed, nextBlue, red_position, blue_position)) continue; 
+                Position nextBlue = !blueEnd ? nextPosition(blue_position, j):blue_position;
+                if(!isRange(nextBlue.x, nextBlue.y) || !isPossible(nextRed, nextBlue, red_position, blue_position)) continue; 
 
                 visited[nextRed.x][nextRed.y][0] = true;
                 visited[nextBlue.x][nextBlue.y][1] = true;
@@ -63,7 +61,7 @@ class Solution {
                 if(map[nextRed.x][nextRed.y] == 3) redEnd = true;
                 if(map[nextBlue.x][nextBlue.y] == 4) blueEnd = true;
 
-                backtracking(nextRed, nextBlue,result+1);
+                min = Math.min(min,backtracking(nextRed, nextBlue,result+1));
 
                 visited[nextRed.x][nextRed.y][0] = false;
                 visited[nextBlue.x][nextBlue.y][1] = false;
@@ -73,17 +71,23 @@ class Solution {
 
             }
         }
+        return min;
     }
 
-
-    // 다음 , 다음, 현재, 현재
-    private  boolean isPossible(Position red, Position blue, Position cntRed, Position cntBlue){
+    private static Position nextPosition(Position p, int dir){
+        return new Position(p.x+dx[dir], p.y+dy[dir]);
+    }
+    private static boolean isRange(int x, int y){
         // 밖으로 못빠지고 벽 아니다.
-        if(red.x<0 || red.y<0 || blue.x<0 || blue.y<0 || 
-           red.x>=map.length || red.y>=map[0].length || blue.x>=map.length || blue.y>=map[0].length || 
-          map[red.x][red.y] == 5 || map[blue.x][blue.y] == 5){
+        if(x<0 || y<0  || x>=map.length || y>=map[0].length 
+           || map[x][y] == 5){
             return false;
         }
+        return true;
+    }
+    // 다음 , 다음, 현재, 현재
+    private static boolean isPossible(Position red, Position blue, Position cntRed, Position cntBlue){
+        
         // 방문 칸 안돼
         if((!redEnd && visited[red.x][red.y][0]) || (!blueEnd && visited[blue.x][blue.y][1])) return false;
         // 동시에 같은 칸 안돼
