4f478eb00c47c90273a6eaab797effefb2b1b688
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-06-13 13:22:23 +0900

    [level 1] Title: 실패율, Time: 38.81 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Fail implements Comparable<Fail>{
+    private int stage;
+    private double rate;
+    
+    Fail(int s, double r){
+        stage = s;
+        rate = r;
+    }
+    
+    public int getStage(){
+        return stage; 
+    }
+    
+    public int compareTo(Fail f){
+        return Double.compare(f.rate, rate) == 0 ? Integer.compare(stage, f.stage) 
+            : Double.compare(f.rate, rate);
+    }
+}
+class Solution {
+    public int[] solution(int N, int[] stages) {
+   
+        int[] failArr = new int[N+2];
+        
+        for(int stage : stages){
+            failArr[stage]++;
+        }
+        
+        List<Fail> list = new ArrayList();
+        
+        double len = stages.length;
+        
+        for(int i=1; i<=N; i++){
+            int num = failArr[i];
+            len -= failArr[i-1];
+            if(num==0){
+                list.add(new Fail(i, 0.0));
+            }else{
+                list.add(new Fail(i, num/len));
+            }
+            
+        }
+    
+        Collections.sort(list);
+        
+        return list.stream().mapToInt(Fail :: getStage).toArray();
+    }
+}
