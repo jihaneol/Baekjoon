de115eb500ca1a12ed086928295dd4a53ce4c307
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-06-19 01:06:21 +0900

    [level 2] Title: 주식가격, Time: 42.13 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Stuck{
+    int idx, price;
+    Stuck(int i, int p){
+        idx = i;
+        price = p;
+    }
+}
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Stuck> stack = new Stack();
+        
+        for(int i=0; i<prices.length; i++){
+            int price = prices[i];
+                
+            while(!stack.isEmpty() && stack.peek().price>price){
+                Stuck pre = stack.pop();
+                answer[pre.idx] = i-pre.idx;
+            }
+            
+            if(stack.isEmpty() || stack.peek().price<=price){
+                stack.push(new Stuck(i, price));
+            }
+        }
+        
+        // 나머지
+        while(!stack.isEmpty()){
+            Stuck now = stack.pop();
+            answer[now.idx] = prices.length - now.idx-1;
+        }
+        return answer;
+    }
+}
