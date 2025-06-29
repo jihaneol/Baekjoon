3ed3ece349a04b2833132d7ab7e8ad82e3509beb
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-06-29 20:11:47 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 28.71 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,45 +1,47 @@
 import java.util.*;
 class Solution {
     private static Map<Integer, Map<String, Integer>> courseMap;
+    
     public String[] solution(String[] orders, int[] course) {
-        
         courseMap = new HashMap();
-        for(int i: course){
-            courseMap.put(i, new HashMap());
+        // 코스요리 개수 courseMap 채우기
+        for(int c : course){
+            courseMap.put(c, new HashMap());
         }
         
-        for(String order:orders){
-            char[] orderArray = order.toCharArray();
-            Arrays.sort(orderArray);
-            combi(0, orderArray, "");
+        
+        // 주문에서 정렬하고 조합으로 코스 요리 수 만큼 map 채우기
+        for(String order : orders){
+            char[] orderArr = order.toCharArray();
+            Arrays.sort(orderArr);
+            combi(0, orderArr, "");
         }
+        List<String> answer = new ArrayList();
         
-        ArrayList<String> answer = new ArrayList();
-        
+        // courseMap에서 코스 종류별로 많이 주문된 메뉴 answer에 담기
         for(Map<String, Integer> count : courseMap.values()){
             count.values()
-                .stream()
-                .max(Comparator.comparingInt(o -> o))
-                .ifPresent(cnt -> count.entrySet()
-                          .stream()
-                           .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1).forEach(entry -> answer.add(entry.getKey()))
-                );
-                           
-                          
+            .stream()
+            .max(Comparator.comparingInt(o->o))
+            .ifPresent(cnt -> count.entrySet().stream()
+                       // 최소 2명 이상이 주문 && 가장 많이 주문
+                       .filter(entry -> cnt>1 && cnt == entry.getValue())
+                       .forEach(entry -> answer.add(entry.getKey()))
+                      );
         }
         Collections.sort(answer);
-        return answer.toArray(new String[0]);
+       return answer.toArray(new String[0]);
         
     }
     public static void combi(int idx, char[] order, String result){
+       // 코스 요리 개수와 같으면
         if(courseMap.containsKey(result.length())){
             Map<String, Integer> map = courseMap.get(result.length());
-            map.put(result, map.getOrDefault(result, 0) + 1);
+            map.put(result, map.getOrDefault(result,0)+1);
         }
         
-        for(int i=idx; i< order.length; i++){
-            // 0, abc , "" -> 1, abc, "a"
-            combi(i+1, order, result+order[i]);
+        for(int i=idx; i<order.length; i++){
+            combi(i+1, order, result + order[i]);
         }
     }
 }
