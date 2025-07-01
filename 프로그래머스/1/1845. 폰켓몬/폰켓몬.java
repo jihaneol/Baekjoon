8128696105cf890587de33e7c739c058512b4e70
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-02 00:32:07 +0900

    [level 1] Title: 폰켓몬, Time: 8.25 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,13 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet();
-        for(int num : nums){
-            set.add(num);
-        }
-        int selectTotal = nums.length/2;
-        int poketmonNum = set.size();
-        if(selectTotal>poketmonNum) return poketmonNum;
+        // n/2 만큼 가져가고 최대로 가질수있는 개수
+        int max = nums.length/2;
+        Set<Integer> set = 
+            Arrays.stream(nums).boxed().collect(Collectors.toCollection(HashSet::new));
         
-        return selectTotal;
+        
+        return max <set.size()?max : set.size();
     }
 }
