933fa056a831c9a211d3f458fba6b654b670239f
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-02 23:00:25 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 9.08 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
 
 ### 성능 요약
 
-메모리: 85.3 MB, 시간: 2.87 ms
+메모리: 86.5 MB, 시간: 9.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 22:18:25
+2025년 07월 02일 23:00:22
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -4,18 +4,18 @@ class Solution {
          int delivery_cnt = 0;
          int pickup_cnt = 0;
         for(int i=n-1; i>-1; i--){
-           
+
             delivery_cnt -= deliveries[i];
             pickup_cnt -= pickups[i];
-            
+
             while(delivery_cnt < 0 || pickup_cnt < 0){
                 delivery_cnt += cap;
                 pickup_cnt += cap;
                 answer+=((i+1)*2);
             }
         }
-        
-       
+
+
         return answer;
     }
 }
