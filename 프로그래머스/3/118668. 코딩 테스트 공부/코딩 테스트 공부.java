f5a220dd108ff1cf647845e61ffc4dcc0776c17e
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-07 06:02:04 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 25.66 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -2,45 +2,49 @@ import java.util.*;
 
 class Solution {
     public int solution(int alp, int cop, int[][] problems) {
-
-         int goal_a=0;
-         int goal_c=0;
-        //목표치를 구하는 for문
-       for(int[] p : problems){
-            goal_a=Math.max(p[0],goal_a);
-            goal_c=Math.max(p[1],goal_c);
+        // 최단 시간
+        // 알고 풀이 능력, 코딩 풀이 능력, 알고 보상, 코딩 보상, 시간 비용
+        int maxa = 0, maxc = 0;
+        // 알고력, 코딩력의 1씩 증가하는걸 dp에 모두 더해준다.
+        for(int[] problem : problems){
+            maxa = Math.max(maxa, problem[0]);
+            maxc = Math.max(maxc, problem[1]);
         }
-
-        if(goal_a<=alp && goal_c<=cop) return 0;
+        if(alp>=maxa) alp = maxa;
+        if(cop>=maxc) cop = maxc;
         
-        alp = Math.min(alp, goal_a);
-        cop = Math.min(cop, goal_c);
-
-        int[][] dp =new int[goal_a+1][goal_c+1];
-
-        for(int i=alp;i<=goal_a;i++){
-            for(int j=cop;j<=goal_c;j++){
-                dp[i][j]=Integer.MAX_VALUE;
+        int[][] dp = new int[maxa+2][maxc+2];
+        
+        for(int i=alp; i<=maxa; i++){
+            for(int j=cop; j<=maxc; j++){
+                  dp[i][j] = Integer.MAX_VALUE;
             }
         }
-
-        dp[alp][cop]=0;
-
-         for(int i=alp;i<=goal_a;i++){
-            for(int j=cop;j<=goal_c;j++){
-                if(i<goal_a) dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j]+1);
-                if(j<goal_c) dp[i][j+1] = Math.min(dp[i][j+1], dp[i][j]+1);
-                
-                for(int[] p : problems){
-                    if(i<p[0] || j<p[1]) continue;
-                    int next_a = Math.min(goal_a, p[2]+i);
-                    int next_c = Math.min(goal_c, p[3]+j);
-                    dp[next_a][next_c] = Math.min(dp[next_a][next_c]
-                                                 ,dp[i][j]+p[4]);
+        
+        // 알고력, 코딩력을 순차적으로 이동한다.
+        // problem에 맞게 dp를 갱신해준다.
+        dp[alp][cop] = 0;
+        for(int i=alp; i<=maxa; i++){
+            for(int j=cop; j<=maxc; j++){
+                for(int[] problem : problems){
+                    // 못풀면 다음
+                    if(i<problem[0] || j<problem[1])
+                        continue;
+                    // 풀었을때 얻는 능력을 갱신
+                    int nextI = i + problem[2] > maxa ? maxa : i + problem[2];
+                    int nextJ = j + problem[3] > maxc ? maxc : j + problem[3];
+                    
+                    dp[nextI][nextJ] = Math.min(dp[nextI][nextJ], 
+                                                              dp[i][j] + problem[4]);
+                    
                 }
+                dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j] + 1);
+                dp[i][j+1] = Math.min(dp[i][j+1], dp[i][j] + 1);
             }
-         }
-        return dp[goal_a][goal_c];
+        }
+        
+        return dp[maxa][maxc];
+
     }
 
 
