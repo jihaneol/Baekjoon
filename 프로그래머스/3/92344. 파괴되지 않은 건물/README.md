c4a5dd970c3810337352db9bec7a998dba748599
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-07 23:51:46 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 49.71 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
@@ -1,10 +1,10 @@
 # [level 3] 파괴되지 않은 건물 - 92344 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344) 
 
 ### 성능 요약
 
-메모리: 216 MB, 시간: 61.70 ms
+메모리: 211 MB, 시간: 49.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 14:43:17
+2025년 07월 07일 23:51:43
 
 ### 문제 설명
 
--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,48 +1,47 @@
 class Solution {
-    static int[][] sum;
-    static int N, M;
 
     public static int solution(int[][] board, int[][] skill) {
-        N = board.length;
-        M = board[0].length;
-
-        sum = new int[N + 1][M + 1];
-        for (int[] s : skill) {
-            int x1 = s[1], y1 = s[2];
-            int x2 = s[3], y2 = s[4];
-            int degree = s[5] * (s[0] == 1 ? -1 : 1);
-
-            sum[x1][y1] += degree;
-            sum[x1][y2 + 1] += (degree * -1);
-            sum[x2 + 1][y1] += (degree * -1);
-            sum[x2 + 1][y2 + 1] += degree;
-        }
-        operate();
-
-        // 살아남은 건물 확인
         int answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (board[i][j] + sum[i][j] > 0) answer++;
+        int n = board.length;
+        int m = board[0].length;
+        //행 열 100만, 스킬 25만
+        // 일 부분만 공격 한다. 1 이면 감소, 2이면 회복
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int[] s : skill){
+            int r1 = s[1];
+            int c1 = s[2];
+            int r2 = s[3];
+            int c2 = s[4];
+            int d = s[5];
+            // 공격
+            if(s[0]==1){
+                d = -d;
             }
+            
+            dp[r1][c1] +=d;
+            dp[r2+1][c2+1] +=d;
+            dp[r1][c2+1] -=d;
+            dp[r2+1][c1] -=d;
+            
         }
-        return answer;
-    }
-
-    // 누적합 계산
-    private static void operate() {
         // 상하
-        for (int y = 1; y < N; y++) {
-            for (int x = 0; x < M; x++) {
-                sum[y][x] += sum[y - 1][x];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                dp[i+1][j] +=dp[i][j];
             }
         }
-        // 좌우
-        for (int x = 1; x < M; x++) {
-            for (int y = 0; y < N; y++) {
-                sum[y][x] += sum[y][x - 1];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j] + dp[i][j] >0){
+                    answer++;
+                }
+                dp[i][j+1] += dp[i][j];
             }
         }
+        
+        return answer;
     }
 
 }
