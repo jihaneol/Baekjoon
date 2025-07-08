4f61651d03dcfa4f3351c6394d3415e5418291df
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-08 13:23:23 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 253.69 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,67 +1,72 @@
 import java.util.*;
 class Solution {
-    private int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-    
-    public class Pos{
-        int id,x,y,next;
-        Pos(int id, int x, int y, int next){
-            this.id = id;
+    class Robot{
+        int x, y, idx, nextPoint;
+        Robot(int x, int y, int idx){
             this.x = x;
             this.y = y;
-            this.next = next;
+            this.idx = idx;
+            this.nextPoint = 1;
         }
     }
-    
+    private int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
-        int lastRouteNum = routes[0].length;
-        Queue<Pos> q = new LinkedList();
+        // r 좌표가 먼저 변하는 이동을 먼저한다.(-1,0)(1,0)(0,-1)(0,1) 맨해튼 거리로 가까워지면
+        Queue<Robot> q = new ArrayDeque();
+        // 충돌 확인 map
+        Map<String, Integer> crashMap = new HashMap();
+        int endPoint = routes[0].length;
+        // q에 로봇 위치 넣기
         for(int i=0; i<routes.length; i++){
-            int s = routes[i][0]-1;
-            q.add(new Pos(i,points[s][0],points[s][1],1));
+            int[] route = routes[i];
+            int x = points[route[0]-1][0];
+            int y = points[route[0]-1][1];
+            q.add(new Robot(x, y, i));
         }
-        int[][] map = new int[101][101];
         
         while(!q.isEmpty()){
-            // 위험 상황 확인.
-            for(Pos now : q){
-                map[now.x][now.y]++;
-                if(map[now.x][now.y]==2){
-                    answer++;
+            int len = q.size();
+            while(len-->0){
+                Robot now = q.poll();
+                String key = now.x+","+now.y;
+                crashMap.put(key, crashMap.getOrDefault(key, 0)+1);
+                
+                // 다음 포인트가 없으면
+                if(now.nextPoint>=endPoint) continue;
+                
+               
+                int[] next = points[routes[now.idx][now.nextPoint]-1];
+           
+                // 아래
+                for(int i=0; i<4; i++){
+                    int nx = now.x + dir[i][0];
+                    int ny = now.y + dir[i][1];
+                        
+                    if(Math.abs(now.x-next[0])+Math.abs(now.y-next[1]) > 
+                       Math.abs(nx-next[0])+Math.abs(ny-next[1]))
+                    {
+                   
+                        now.x = nx;
+                        now.y = ny;
+                        // 다음 위치가 point 면
+                        if(next[0]==nx && next[1]==ny){
+                            now.nextPoint+=1;
+                        }
+                        q.add(now);
+                        break;
+                    }
                 }
             }
-            //이동
-            int size= q.size();
-            while(size--!=0){
-                Pos now = q.poll();
-                map[now.x][now.y]--;
-                //마지막 위치면 끝.
-                if(now.next == lastRouteNum){
-                    continue;
+            // 충돌 확인
+            for(int i : crashMap.values()){
+                if(i>1){
+                    answer ++;
                 }
-                Pos next = getOptimalPos(now, points[routes[now.id][now.next]-1]);
-                q.add(next);
             }
+            crashMap.clear();
         }
-      
+        
         return answer;
-    }
-    
-    public Pos getOptimalPos(Pos now, int[] points){
-        int nowDist = Math.abs(points[0]-now.x) + Math.abs(points[1]-now.y);
-        Pos result = null;
-        for(int i=0; i<4; i++){
-            int nx = now.x + dir[i][0];
-            int ny = now.y + dir[i][1];
-            
-            if(nowDist>Math.abs(points[0]-nx) + Math.abs(points[1]-ny)){
-                result = new Pos(now.id,nx,ny,now.next);
-                if(nowDist==1){
-                    result.next++;
-                }
-                break;
-            }
-        }
-        return result;
     }
 }
