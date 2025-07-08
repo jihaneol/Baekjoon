70edd8e7d138d46123893e6713ab593924a3ead9
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-08 11:59:15 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.91 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,41 +1,41 @@
+import java.util.*;
 class Solution {
-    public String solution(String video_len, String pos, String op_start, 
-                           String op_end, String[] commands) {
-       
-        int videoTime = makeTime(video_len);
-        int nowTime = makeTime(pos);
-        int s = makeTime(op_start);
-        int e = makeTime(op_end);
-        for(String comm : commands){
-            // 오프닝 구간
-            if(s<=nowTime && nowTime<=e){
-                nowTime = e;
-            }
-            if(comm.equals("next")){
-                nowTime = nowTime+10>=videoTime? videoTime:nowTime+10;
-            }else{
-                nowTime = nowTime-10<=0? 0:nowTime-10;
-            }
-            
-            // 오프닝 구간
-            if(s<=nowTime && nowTime<=e){
-                nowTime = e;
-            }
-        }
-        int h = nowTime/60;
-        int m = nowTime%60;
-      
-        return timeToString(h) + ":" + timeToString(m);
-    }
-    public int makeTime(String time){
-        String[] t = time.split(":");
-        return Integer.parseInt(t[0])*60 + Integer.parseInt(t[1]);
-    }
-    public String timeToString(int t){
-        if(t>9){
-            return String.valueOf(t);    
-        }
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String answer = "";
+    
+        // 시간 -> 정수로 변환
+        int start = timeToInt(op_start);
+        int end = timeToInt(op_end);
+        int now = timeToInt(pos);
+        int len = timeToInt(video_len);
         
-        return "0"+String.valueOf(t);
+        for(String command : commands){
+            // 오프닝 위치인지 확인
+            if(start<= now && now < end){
+                now = end;
+            }
+            if("next".equals(command)){
+                // 동영상 길이 넘으면 동영상 마지막 위치로
+                now = now + 10 <= len ? now + 10 : len; 
+            }else{
+                // 0 이하면 0으로
+                now = now - 10 >= 0 ? now - 10 : 0;
+            }
+        }
+        // 오프닝 위치인지 확인
+            if(start<= now && now < end){
+                now = end;
+            }
+        
+        return String.format("%02d:%02d", now/60, now%60);
     }
+    public int timeToInt(String time){
+        String[] split = time.split(":");
+        int min = Integer.parseInt(split[0])*60;
+        int sec = Integer.parseInt(split[1]);
+        return min + sec; 
+    }
+//     public String IntToTime(int time){
+        
+//     }
 }
