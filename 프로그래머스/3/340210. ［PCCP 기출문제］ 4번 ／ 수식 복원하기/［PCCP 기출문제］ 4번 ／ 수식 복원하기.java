1fbd57bdda0009172e67b7de881238383f80836d
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-18 15:50:15 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 19.89 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -2,67 +2,76 @@ import java.util.*;
 class Solution {
     
     public String[] solution(String[] expressions) {
-        List<Integer> radixList = new ArrayList();
-        for(int base = 2; base<10; base++){
-            if(validate(expressions, base)){
-                radixList.add(base);
+     
+        List<String> xList = new ArrayList();
+        boolean[] isPossible = new boolean[10];
+        Arrays.fill(isPossible, true);
+        
+        for(String expression : expressions){
+            String[] split = expression.split(" = ");
+            if("X".equals(split[1])){
+                xList.add(split[0]);
+            }
+            for(int i=2; i<10; i++){
+                if(!isPossible[i]) continue;
+                try{
+                    String[] op = split[0].split(" ");
+                    int a = Integer.parseInt(op[0],i);
+                    int b = Integer.parseInt(op[2],i);
+                    if("X".equals(split[1])){
+                        break;
+                    }
+                    int c = 0;
+                    if("+".equals(op[1])){
+                        c = a + b;
+                    }else{
+                        c = a - b;
+                    }
+                    
+                    if(!split[1].equals(Integer.toString(c, i))){
+                        isPossible[i] = false;
+                    }
+                    
+                }catch(Exception e){
+                    isPossible[i] = false;
+                }    
             }
         }
         List<String> answer = new ArrayList();
-        for(String expression : expressions){
-            String[] split = expression.split("=");
-            
-            if("X".equals(split[1].trim())){
-                
-                Set<String> radixSet = new HashSet();
-                String[] exp = split[0].split(" ");
-                String res = "";
-                for(int base : radixList){
-                    res = Integer.toString(calcu(exp, base),base);
-                    radixSet.add(res);
-                  
-                }
        
-                if(radixSet.size()==1){
-                    answer.add(split[0]+"= "+res);
-                }else{
-                    answer.add(split[0]+"= ?");
-                }
+        for(String exp : xList){
+            String[] op = exp.split(" ");
+            String x = null;
+            Set<String> set = new HashSet();
+            for(int i=2; i<10; i++){
+                if(!isPossible[i]) continue;
+                try{
+                    int a = Integer.parseInt(op[0],i);
+                    int b = Integer.parseInt(op[2],i);
+                    int c = 0;
+                    if("+".equals(op[1])){
+                        c = a + b;
+                    }else{
+                        c = a - b;
+                    }
+                    x = Integer.toString(c, i);
+                    set.add(x);
+                }catch(Exception e){
+                }    
+                
                 
             }
+            
+            if(set.size()==1){
+                answer.add(exp+" = "+x);
+            }else{
+                answer.add(exp+" = ?");
+            }
+                
         }
+    
         
         return answer.stream().toArray(String[]::new);
-    }
-    private boolean validate(String[] expressions, int base){
-        
-        for(String expression : expressions){
-            
-            for(int i=0; i<expression.length(); i++){
-                char c = expression.charAt(i);
-                if(Character.isDigit(c) && base <= c -'0') return false;
-            }
-            
-            String[] split = expression.split("=");
-            String res = split[1].trim();
-            
-            if(!"X".equals(res)){
-                int target = calcu(split[0].split(" "), base);
-                if(Integer.parseInt(res,base) != target) return false;
-            }
-        }
-        return true;
-    }
-    private int calcu(String[] exp, int base){
-        String a = exp[0];
-        String op = exp[1];
-        String b = exp[2];
-        
-        if("+".equals(op)){
-            return Integer.parseInt(a, base) + Integer.parseInt(b,base);
-        }else{
-            return Integer.parseInt(a, base) - Integer.parseInt(b,base);
-        }
     }
     
 }
