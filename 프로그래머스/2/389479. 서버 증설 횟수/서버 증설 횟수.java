1f8569b1db337354a22012ed2a715eb020fe2637
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-07-23 14:58:51 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,24 +1,20 @@
-import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
-       
-        int answer = 0; // 증설 횟수
-        int[] servers = new int[24];
-        
-        for(int i=0; i<24; i++){
-            int player = players[i]; // 게임 이용자의 수
-            int n = servers[i];      // 증설된 서버의 수
-            
-            int need = player/m - n; // 필요한 서버 수
-            
-            if(need > 0){
-                for(int j=0; j<k; j++){
-                    if(i+j>=24) break;
-                    servers[i+j] += need;
+        int answer = 0;
+        int[] servers = new int[players.length+k+1];
+        for(int i=0; i<players.length; i++){
+            int player = players[i]; // 현재 인원
+            int n = servers[i]*m + m; // 현재 감당 가능한 인원.
+            if(player >= n){
+                // 증설 개수
+                int scale = ((player-n)/m+1);
+                for(int j=i; j<i+k; j++){
+                    // 필요한 서버수
+                    servers[j] += scale;
                 }
-                answer += need;
+                answer+=scale;
             }
-
+            
         }
         return answer;
     }
