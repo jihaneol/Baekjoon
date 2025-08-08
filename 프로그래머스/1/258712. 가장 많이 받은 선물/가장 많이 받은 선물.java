d25104fe12bb5b3068c55394fc53d09242756ead
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-08-08 16:41:54 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 46.46 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,41 +1,64 @@
 import java.util.*;
 class Solution {
-    
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        int n = friends.length;
-        Map<String, Integer> friendIndex = new HashMap();
-        int[][] friendMap = new int[n][n];
-        int[] giftScore = new int[n];
-        
-        for(int i=0; i<n; i++){
-            friendIndex.put(friends[i],i);
+     
+        // 선물을 가장 많이 받을 친구의 선물 수 구하기
+        // 선물 지수 맵
+        Map<String, Integer> scoreMap = new HashMap();
+        // 관계도
+        Map<String, Integer> relationMap = new HashMap();
+        for(String f : friends){
+            scoreMap.put(f, 0);
         }
-        
-        for(String g : gifts){
-            String[] giftName = g.split(" ");
-            int aIndex = friendIndex.get(giftName[0]);
-            int bIndex = friendIndex.get(giftName[1]);
+        for(String gift : gifts){
+            String[] split = gift.split(" ");
+            String a = split[0];
+            String b = split[1];
             
-            giftScore[aIndex]++;
-            giftScore[bIndex]--;
-            friendMap[aIndex][bIndex]++;
+            relationMap.put(gift, relationMap.getOrDefault(gift,0)+1);
+            
+            scoreMap.put(a, scoreMap.get(a)+1);
+            scoreMap.put(b, scoreMap.get(b)-1);
         }
-        
-        for(int i=0; i<n; i++){
-            int num = 0;
-            for(int j=0; j<n; j++){
-                if(i==j) continue;
-              
-                if(friendMap[i][j] > friendMap[j][i] ||
-                   (friendMap[i][j] == friendMap[j][i] && giftScore[i] > giftScore[j])){
-                    num++;
+        int n = friends.length;
+        int[] answer = new int[n];
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                String a = friends[i];
+                String b = friends[j];
+                
+                // 기록이 있는지 확인
+                int relation_a = relationMap.getOrDefault(a+" "+b,-1);
+                int relation_b = relationMap.getOrDefault(b+" "+a,-1);
+                if(relation_a!=-1 || relation_b!=-1){ // 기록이 있다.
+            
+                    if(relation_a>relation_b) answer[i]++;
+                    else if(relation_b>relation_a) answer[j]++;
+                    else{
+                         int score_a = scoreMap.get(a);
+                            int score_b = scoreMap.get(b);
+                            if(score_a>score_b){
+                                answer[i]++;
+                            }else if(score_b>score_a){
+                                answer[j]++;
+                            }
+                    }
+                }else{
+                      // 기록이 없다면 지수확인..
+                // 지수 확인
+                int score_a = scoreMap.get(a);
+                int score_b = scoreMap.get(b);
+                if(score_a>score_b){
+                    answer[i]++;
+                }else if(score_a<score_b){
+                    answer[j]++;
                 }
+                }
+              
             }
-            answer = Math.max(answer,num);
         }
-        return answer;
-    }
     
-        
+   
+        return Arrays.stream(answer).max().orElse(-1);
+    }
 }
