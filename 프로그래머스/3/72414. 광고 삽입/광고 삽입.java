a7e3fc9e8dd7d0eafa728400d6ed22726d06093b
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-08-11 12:50:12 +0900

    [level 3] Title: 광고 삽입, Time: 3731.45 ms, Memory: 310 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,60 +1,52 @@
+import java.util.Arrays;
+
 class Solution {
+
     public String solution(String play_time, String adv_time, String[] logs) {
-        // 36만이하 sliding window 하면 되겠는데.. 0~내 길이 adv_time
-        // -> 끝까지 해서  가장큰 시작위치 찾으면 끝..
-        int n = timeToInt(play_time);
-        int len = timeToInt(adv_time);
-        int[] playArr = new int[n+1];
-        
-        // 로그 길이 30만
-        for(String log : logs){
-            String[] split = log.split("-");
-            int s = timeToInt(split[0]);
-            int e = timeToInt(split[1]);
-            playArr[s]++;
-            playArr[e]--;
-        }
-        
-        for(int i=1; i<=n; i++){
-            playArr[i] +=playArr[i-1];
-        }
-        
-      
-        int s_max = 0; // 공익 광고 시작 시간
-        long pre = 0; // 이전 최대 시간
-        for(int i=0; i<len; i++){
-            pre+=playArr[i];
-        }
-        long max = pre; // 공익 광고 최대 시간
-        
-        // 슬라이딩 윈도우
-        for(int s=1; s<=n-len; s++){
-            int e = s+len;
-            long now = pre-playArr[s-1] + playArr[e-1]; 
-            
-            if(now>max){
-                max=now;
-                s_max = s;
+        int playTime = toIntTime(play_time);
+        int advTime = toIntTime(adv_time);
+        int[] times = new int[360_000];
+        for (String log : logs) {
+            String[] splitLog = log.split("-");
+            int startTime = toIntTime(splitLog[0]);
+            int endTime = toIntTime(splitLog[1]);
+            for (int i = startTime; i < endTime; i++) {
+                times[i]++;
             }
-            pre = now;
         }
-        
-        return intToTime(s_max);
+
+        int maxIdx = 0;
+        long totalTime = 0;
+        for (int i = 0; i <= advTime; i++) {
+            totalTime += times[i];
+        }
+        long maxTotalTime = totalTime;
+        for (int i = advTime; i < playTime; i++) {
+            totalTime += times[i] - times[i - advTime];
+            if (totalTime > maxTotalTime) {
+                maxTotalTime = totalTime;
+                maxIdx = i - advTime + 1;
+            }
+        }
+
+        return toStrTime(maxIdx);
     }
-    public int timeToInt(String time){
-        String[] split = time.split(":");
-        int h = Integer.parseInt(split[0]) * 60 * 60;
-        int m = Integer.parseInt(split[1]) * 60;
-        int s = Integer.parseInt(split[2]);
-        
-        return h + m + s;
+
+    int toIntTime(String strTime) {
+        int[] HMS = Arrays.stream(strTime.split(":"))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+        return 3600 * HMS[0] + 60 * HMS[1] + HMS[2];
     }
-    public String intToTime(int time){
-        int h = time/3600;
-        time%=3600;
-        int m = time/60;
-        int s = time%60;
-        
-        return String.format("%02d:%02d:%02d",h,m,s);
+
+    String toStrTime(int time) {
+        int H = time / 3600;
+        int M = (time - 3600 * H) / 60;
+        int S = time - 3600 * H - 60 * M;
+
+        return (H < 10 ? "0" : "") + H + ":" +
+            (M < 10 ? "0" : "") + M + ":" +
+            (S < 10 ? "0" : "") + S;
     }
 }
