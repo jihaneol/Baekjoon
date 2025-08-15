dec91ec0f6c4e7dc192837dc7cf65312aaf703ec
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-08-15 20:09:42 +0900

    [level 1] Title: 유연근무제, Time: 0.69 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,34 +1,31 @@
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
         int answer = 0;
-        // 0, 6 은 무시
-        for(int s=0; s<schedules.length; s++){
-            int start = convert(schedules[s]);
-            int[] log = timelogs[s];
-            boolean flag = true;
-            for(int t=0; t<7; t++){
-                if((t+startday)%7==6 || (t+startday)%7==0)
-                    continue;
-                
-                if(log[t]>start){
+        // (startday+i)%7 == 6, 0 넘어가. 
+        for(int i=0; i<schedules.length; i++){
+            int startTime = schedules[i];
+            boolean flag = true; // 상 받을 수 있는지
+            int[] log = timelogs[i];
+            for(int j=0; j<7; j++){
+                if((j+startday)%7==6 || (j+startday)%7==0) continue;
+
+                if(change(startTime+10)<log[j]){
                     flag = false;
                     break;
                 }
             }
+            
             if(flag) answer++;
         }
-        
         return answer;
     }
-    private int convert(int time){
+    public int change(int time){
+        int m = time%100;
         int h = time/100;
-        int m = time%100+10;
-        
         if(m>=60){
             h++;
-            m%=60;
+            m-=60;
         }
-        
-        return h*100 + m; 
+        return h*100 + m;
     }
 }
