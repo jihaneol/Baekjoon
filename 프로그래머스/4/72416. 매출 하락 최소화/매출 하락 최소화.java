f7a4cfe835dc04d487c8962e2a79fa53b6eb972e
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-03 01:52:33 +0900

    [level 4] Title: 매출 하락 최소화, Time: 214.17 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.java
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.java
@@ -1,55 +1,49 @@
 import java.util.*;
 class Solution {
-    
-    private List<List<Integer>> tree = new ArrayList();
+    private List<Integer>[] tree;
     private int[][] dp;
     public int solution(int[] sales, int[][] links) {
-        int answer = 0;
         int n = sales.length;
+        tree = new List[n+1];
         dp = new int[n+1][2];
-        for(int i=0; i<=n; i++){
-            tree.add(new ArrayList());
+        for(int i=1; i<=n; i++){
+            tree[i] = new ArrayList();
         }
         
-        
-        for(int i=0; i<n-1; i++){
-            int[] link = links[i];
-            int s = link[0];
-            int e = link[1];
-            
-            tree.get(s).add(e);
+        for(int[] link : links){
+            tree[link[0]].add(link[1]);
         }
-        getWorkShop(1, sales);
-        return Math.min(dp[1][0], dp[1][1]);
+        dfs(sales, 1);
+        
+        return Math.min(dp[1][0],dp[1][1]);
     }
-    private void getWorkShop(int cur, int[] sales){
+    private void dfs(int[] sales, int start){
+        boolean isLeaf = true;
+        boolean noChild = true;
+        dp[start][1] = sales[start-1];
         
-        int cur_sale = sales[cur-1];
-        dp[cur][1] = cur_sale;
-        
-        boolean noChildInclude = true;
-        boolean leaf = true;
-        
-        for(int next : tree.get(cur)){
-            leaf = false;
-            getWorkShop(next, sales);
-            if(dp[next][0] >= dp[next][1]){
-                noChildInclude = false;
+        for(int next : tree[start]){
+            isLeaf = false;
+            dfs(sales, next);
+            
+            // 팀원을 하나라도 선택했다면
+            if(dp[next][1]<=dp[next][0]){
+                noChild = false;
             }
-            dp[cur][1] += Math.min(dp[next][0], dp[next][1]);
-            dp[cur][0] += Math.min(dp[next][0], dp[next][1]);
+            
+            dp[start][0] += Math.min(dp[next][0], dp[next][1]); 
+            dp[start][1] += Math.min(dp[next][0], dp[next][1]); // 팀장을 선택하니까. 자식의 부하들의 최소값을 더한다.
         }
-     
         
-        
-        if(!leaf && noChildInclude){
-            int sum = Integer.MAX_VALUE;
-            for(int next : tree.get(cur)){
-                if(sum > dp[next][1] - dp[next][0]){
-                    sum = dp[next][1] - dp[next][0];
+        if(!isLeaf && noChild){
+            int min = Integer.MAX_VALUE;
+            for(int next : tree[start]){
+                if(min>dp[next][1] - dp[next][0]){
+                    min = dp[next][1] - dp[next][0];
                 }
             }
-            dp[cur][0] += sum;
+            dp[start][0] += min;
         }
+      
     }
 }
