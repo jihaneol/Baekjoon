bea5411ffa974d49093b8a2731e4629b5089f9e8
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-03 03:35:08 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 301.89 ms, Memory: 257 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -1,102 +1,101 @@
+import java.util.*;
 class Solution {
-    private boolean[][][] visited; // 빨간, 파랑 수레 이동 체크
-    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-    private int n, m, answer;
-    private Position red_end, blue_end;
-    private class Position{
+    class Pos {
         int x, y;
-        Position(int x, int y){
+        Pos(int x, int y){
             this.x = x;
             this.y = y;
         }
-        public boolean equals(Position p){
-            return x==p.x && y==p.y;
-        }
     }
-    
+    private boolean red_arrive, blue_arrive;
+    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    private int n, m, answer;
+    private boolean[][][] visited; // [0] 는 red, [1] 는 blue
+    private int red_x, red_y, blue_x, blue_y;
+    private int[][] g_maze;
     public int solution(int[][] maze) {
+        g_maze = maze;
+        answer = 21;
         n = maze.length;
         m = maze[0].length;
-        answer = 16;
-        visited = new boolean[2][n][m];
-        Position red = null;
-        Position blue = null;
+        
+        visited = new boolean[n][m][2];
+        
+        Pos red = null;
+        Pos blue = null;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                switch(maze[i][j]){
-                    case 1:
-                        red = new Position(i, j);
-                        visited[0][i][j] = true;
-                        break;
-                    case 2:
-                        blue = new Position(i,j);
-                        visited[1][i][j] = true;
-                        break;
-                    case 3:
-                        red_end = new Position(i, j);
-                        break;
-                    case 4:
-                        blue_end = new Position(i,j);
-                }     
-            }
-        }
-        backTracking(red, blue, 0, maze);
-            
-        return answer==16 ? 0 : answer;
-    }
-    public void backTracking(Position red, Position blue, 
-                             int total ,int[][] maze)
-    {
-
-        if(red.equals(red_end) && blue.equals(blue_end)){
-            if(total < answer){
-                answer = total;
-            }
-            return;
-        }
-    
-
-        // 완탐
-        for(int i=0; i<4; i++){
-                Position nextRed = red.equals(red_end) ? 
-                    red : new Position(red.x+dir[i][0], red.y+dir[i][1]);
-          
-            for(int j=0; j<4; j++){
-                Position nextBlue = blue.equals(blue_end) ? 
-                    blue : new Position(blue.x+dir[j][0], blue.y+dir[j][1]);
-                
-                if(checkout(nextRed, nextBlue, red, blue, maze)){
-                    visited[0][nextRed.x][nextRed.y] = true;
-                    visited[1][nextBlue.x][nextBlue.y] = true;
-                    backTracking(nextRed, nextBlue, total+1, maze);
-                    visited[0][nextRed.x][nextRed.y] = false;
-                    visited[1][nextBlue.x][nextBlue.y] = false;
+                if(maze[i][j]==1){
+                    red = new Pos(i,j);
+                }else if(maze[i][j]==2){
+                    blue = new Pos(i,j);
+                }else if(maze[i][j]==3){
+                    red_x = i;
+                    red_y = j;
+                }else if(maze[i][j]==4){
+                    blue_x = i;
+                    blue_y = j;
                 }
             }
         }
+        visited[red.x][red.y][0] = true;
+        visited[blue.x][blue.y][1] = true;
+        move(red.x, red.y, blue.x, blue.y, 0);
+        
+        // 어떻게 도착이 풀리는지 알지?
+        
+        return answer==21?0:answer;
+    }
+    
+    private void move(int rx, int ry, int bx, int by, int round){
+      
+    
+        
+        if(blue_arrive && red_arrive){
+            answer = Math.min(answer, round);
+            return;
+        }
+        
+        for(int i=0; i<4; i++){
+            Pos nextRed = red_arrive? new Pos(rx, ry) : new Pos(rx+dir[i][0], ry+dir[i][1]);
+            for(int j=0; j<4; j++){
+                Pos nextBlue = blue_arrive? new Pos(bx,by) : new Pos(bx+dir[j][0], by+dir[j][1]);
+                
+                if(!validate(new Pos(rx,ry), new Pos(bx,by), nextRed, nextBlue)) continue;
+                
+                if(!red_arrive && visited[nextRed.x][nextRed.y][0]) continue;
+                if(!blue_arrive && visited[nextBlue.x][nextBlue.y][1]) continue;
+                
+                if(g_maze[nextRed.x][nextRed.y] == 3) red_arrive = true;
+                if(g_maze[nextBlue.x][nextBlue.y] == 4) blue_arrive = true;
+                
+                
+                visited[nextRed.x][nextRed.y][0] = true;
+                visited[nextBlue.x][nextBlue.y][1] = true;
+                move(nextRed.x, nextRed.y, nextBlue.x, nextBlue.y, round+1);
+                visited[nextRed.x][nextRed.y][0] = false;
+                visited[nextBlue.x][nextBlue.y][1] = false;  
+                
+                red_arrive = false;
+                blue_arrive = false;
+            }
+        }
+        
+       
         
     }
-    public boolean checkout(Position red, Position blue, Position preRed, Position preBlue,
-                            int[][] maze){
-        // 판 밖으로는 안돼
-        if(red.x<0 || red.x>=n || red.y<0 || red.y>=m ||
-          blue.x<0 || blue.x>=n || blue.y<0 || blue.y>=m){
-         
-            return false;
-        }
-        // 벽은 안돼
-        if(maze[red.x][red.y]==5 || maze[blue.x][blue.y]==5)
-            return false;
-        // 이미 방문한 칸은 안돼 -> 이미 완료한 수레는 배제 해야한다..
-        if((!red.equals(red_end) && visited[0][red.x][red.y]) || 
-           (!blue.equals(blue_end) && visited[1][blue.x][blue.y]))
-            return false;
-        // 동시에 같은 칸은 안돼
-        if(red.equals(blue))
-            return false;
-        // 동시에 자리를 바꿀 수 없다.
-        if(red.equals(preBlue) && blue.equals(preRed))
-            return false;
+    private boolean validate(Pos preRed, Pos preBlue, Pos red, Pos blue){
+        // 벽 또는 격자 밖 안된다.
+        if(red.x<0 || red.x>=n || blue.x<0 || blue.x>=n ||
+          blue.y<0 || blue.y>=m || red.y<0 || red.y>=m) return false;
+        
+        if(g_maze[red.x][red.y]==5 || g_maze[blue.x][blue.y]==5) return false;
+        
+        // 동시에 두 수레를 같은 칸으로 움직 X
+        if(red.x==blue.x && red.y==blue.y) return false;
+        
+        // 수레끼리 자리를 바꾸면 안된다.
+        if(preRed.x==blue.x && preRed.y==blue.y && preBlue.x==red.x && preBlue.y==red.y) return false;
         
         return true;
     }
