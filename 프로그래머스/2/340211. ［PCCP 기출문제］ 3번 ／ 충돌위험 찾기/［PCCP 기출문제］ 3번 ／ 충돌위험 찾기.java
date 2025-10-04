6a4a376cdaea6b9242c37f064fc0b02189103251
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-04 23:26:39 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 131.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,72 +1,77 @@
 import java.util.*;
 class Solution {
     class Robot{
-        int x, y, idx, nextPoint;
-        Robot(int x, int y, int idx){
+        int x, y, no, next;
+        Robot(int x, int y, int no){
             this.x = x;
             this.y = y;
-            this.idx = idx;
-            this.nextPoint = 1;
+            this.no = no;
+            this.next = 1;
+        }
+        public void setDir(int x, int y){
+            this.x = x;
+            this.y = y;
         }
     }
     private int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
-        // r 좌표가 먼저 변하는 이동을 먼저한다.(-1,0)(1,0)(0,-1)(0,1) 맨해튼 거리로 가까워지면
+        int m = routes[0].length; // next 가 m을 넘으면 끝
         Queue<Robot> q = new ArrayDeque();
-        // 충돌 확인 map
-        Map<String, Integer> crashMap = new HashMap();
-        int endPoint = routes[0].length;
-        // q에 로봇 위치 넣기
-        for(int i=0; i<routes.length; i++){
-            int[] route = routes[i];
-            int x = points[route[0]-1][0];
-            int y = points[route[0]-1][1];
-            q.add(new Robot(x, y, i));
+        int[][] map = new int[101][101];
+        for(int id=0; id<routes.length; id++){
+            int[] route = routes[id];
+            int[] point = points[route[0]-1];
+            int x = point[0];
+            int y = point[1];
+            
+            q.add(new Robot(x, y, id));
+            map[x][y]++;
         }
         
         while(!q.isEmpty()){
-            int len = q.size();
-            while(len-->0){
-                Robot now = q.poll();
-                String key = now.x+","+now.y;
-                crashMap.put(key, crashMap.getOrDefault(key, 0)+1);
+            int size = q.size();
+            // 충돌 계산
+            answer+=clushCal(map);
+            while(size-->0){
+                Robot now = q.poll();    
+                if(now.next >=m) continue;
                 
-                // 다음 포인트가 없으면
-                if(now.nextPoint>=endPoint) continue;
-                
-               
-                int[] next = points[routes[now.idx][now.nextPoint]-1];
-           
-                // 아래
-                for(int i=0; i<4; i++){
-                    int nx = now.x + dir[i][0];
-                    int ny = now.y + dir[i][1];
-                        
-                    if(Math.abs(now.x-next[0])+Math.abs(now.y-next[1]) > 
-                       Math.abs(nx-next[0])+Math.abs(ny-next[1]))
-                    {
-                   
-                        now.x = nx;
-                        now.y = ny;
-                        // 다음 위치가 point 면
-                        if(next[0]==nx && next[1]==ny){
-                            now.nextPoint+=1;
+                for(int[] d : dir){
+                    int nx = now.x + d[0];
+                    int ny = now.y + d[1];
+                    
+                    if(nx<1 || ny<1 || nx>100 || ny>100) continue;
+                    
+                    // 이동할 포인트
+                    int[] point = points[routes[now.no][now.next]-1];
+                    int nowDir = Math.abs(now.x-point[0])+Math.abs(now.y-point[1]);
+                    int nextDir = Math.abs(nx-point[0])+Math.abs(ny-point[1]);
+                    if(nowDir > nextDir){
+                        if(nextDir == 0){
+                            now.next++;
                         }
+                        map[nx][ny]++;
+                        now.setDir(nx,ny);
                         q.add(now);
                         break;
                     }
                 }
             }
-            // 충돌 확인
-            for(int i : crashMap.values()){
-                if(i>1){
-                    answer ++;
-                }
-            }
-            crashMap.clear();
         }
         
         return answer;
+    }
+    private int clushCal(int[][] map){
+        int result = 0;
+        for(int i=1; i<101; i++){
+            for(int j=1; j<101; j++){
+                if(map[i][j]>1){
+                    result++;
+                }
+                map[i][j] = 0;
+            }
+        }
+    return result;
     }
 }
