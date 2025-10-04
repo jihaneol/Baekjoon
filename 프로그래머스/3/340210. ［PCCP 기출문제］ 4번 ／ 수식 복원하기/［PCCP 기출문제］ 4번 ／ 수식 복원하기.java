d16fe8ee3b5e3c330d418ef8d0c0ff736f94c41e
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-04 22:24:48 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 22.13 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -1,77 +1,75 @@
 import java.util.*;
 class Solution {
-    
     public String[] solution(String[] expressions) {
-     
         List<String> xList = new ArrayList();
         boolean[] isPossible = new boolean[10];
         Arrays.fill(isPossible, true);
         
-        for(String expression : expressions){
+        for(String expression: expressions){
+            
             String[] split = expression.split(" = ");
-            if("X".equals(split[1])){
+            if("X".equals(split[1])) {
                 xList.add(split[0]);
             }
+            String[] exp = split[0].split(" ");
             for(int i=2; i<10; i++){
                 if(!isPossible[i]) continue;
+                
                 try{
-                    String[] op = split[0].split(" ");
-                    int a = Integer.parseInt(op[0],i);
-                    int b = Integer.parseInt(op[2],i);
-                    if("X".equals(split[1])){
-                        break;
-                    }
-                    int c = 0;
-                    if("+".equals(op[1])){
-                        c = a + b;
+                    int a = Integer.parseInt(exp[0],i);
+                    int b = Integer.parseInt(exp[2],i);
+                    int x = 0;
+                    if("-".equals(exp[1])){
+                        x = a - b;
                     }else{
-                        c = a - b;
+                        x = a + b;
                     }
                     
-                    if(!split[1].equals(Integer.toString(c, i))){
+                    if(!"X".equals(split[1]) && !split[1].equals(Integer.toString(x,i))){
                         isPossible[i] = false;
                     }
                     
                 }catch(Exception e){
                     isPossible[i] = false;
-                }    
+                }
             }
+            
         }
         List<String> answer = new ArrayList();
-       
-        for(String exp : xList){
-            String[] op = exp.split(" ");
-            String x = null;
+        
+        for(String expression : xList){
+            String[] exp = expression.split(" ");
+            String result = null;
             Set<String> set = new HashSet();
             for(int i=2; i<10; i++){
                 if(!isPossible[i]) continue;
+                
                 try{
-                    int a = Integer.parseInt(op[0],i);
-                    int b = Integer.parseInt(op[2],i);
-                    int c = 0;
-                    if("+".equals(op[1])){
-                        c = a + b;
+                    int a = Integer.parseInt(exp[0],i);
+                    int b = Integer.parseInt(exp[2],i);
+                    int x = 0;
+                    if("-".equals(exp[1])){
+                        x = a - b;
                     }else{
-                        c = a - b;
+                        x = a + b;
                     }
-                    x = Integer.toString(c, i);
-                    set.add(x);
+                    result = Integer.toString(x,i);
+                    set.add(result);
+                    // System.out.println(i+" "+result);
+                    
                 }catch(Exception e){
-                }    
-                
-                
+                }
             }
             
+            System.out.println("d");
             if(set.size()==1){
-                answer.add(exp+" = "+x);
+                answer.add(expression+" = "+result);
             }else{
-                answer.add(exp+" = ?");
+                answer.add(expression+" = ?");
             }
-                
         }
-    
+        
         
         return answer.stream().toArray(String[]::new);
     }
-    
 }
