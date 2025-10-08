9a70c788d9418d96b0392691ec354917bb9cb0d8
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-09 01:17:56 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 42.10 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,48 +1,53 @@
 import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-     
-        // 선물을 가장 많이 받을 친구의 선물 수 구하기
-        // 선물 지수 맵
+      
+        // 선물 지수 = 준 선물 수 - 받은 선물 수
+        // 더 많이 선물 받은 사람이 다음에 선물을 받는다.
+        Map<String, Integer> tradeMap = new HashMap();
         Map<String, Integer> scoreMap = new HashMap();
-        // 관계도
-        Map<String, Integer> relationMap = new HashMap();
+        
         for(String f : friends){
             scoreMap.put(f, 0);
         }
+        
         for(String gift : gifts){
+            tradeMap.put(gift, tradeMap.getOrDefault(gift, 0)+1);
             String[] split = gift.split(" ");
-            String a = split[0];
-            String b = split[1];
             
-            relationMap.put(gift, relationMap.getOrDefault(gift,0)+1);
-            
-            scoreMap.put(a, scoreMap.get(a)+1);
-            scoreMap.put(b, scoreMap.get(b)-1);
+            scoreMap.put(split[0], scoreMap.getOrDefault(split[0],0)+1);
+            scoreMap.put(split[1], scoreMap.getOrDefault(split[1],0)-1);
         }
         int n = friends.length;
-        int answer = 0;
-        for(int i=0; i<n; i++){
-            int sum = 0;
-            for(int j=0; j<n; j++){
-                if(i==j) continue;
+        
+        int[] answer = new int[n];
+        
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
                 String a = friends[i];
                 String b = friends[j];
                 
-                // 기록이 있는지 확인
-                int relation_a = relationMap.getOrDefault(a+" "+b,-1);
-                int relation_b = relationMap.getOrDefault(b+" "+a,-1);
-                int score_a = scoreMap.get(a);
-                int score_b = scoreMap.get(b);
+                // 거래 내역 확인하기.
+                int tradeA = tradeMap.getOrDefault(a+" "+b, 0);
+                int tradeB = tradeMap.getOrDefault(b+" "+a, 0);
                 
-                if(relation_a>relation_b || (relation_a==relation_b && score_a>score_b)){
-                    sum++;
+                if((tradeB==0 && tradeA==0)||tradeA==tradeB){
+                    int scoreA = scoreMap.get(a);
+                    int scoreB = scoreMap.get(b);
+                    if(scoreA>scoreB){
+                        answer[i]++;
+                    }else if(scoreB>scoreA){
+                        answer[j]++;
+                    }
+                }else if(tradeA>tradeB){
+                    answer[i]++;
+                }else{
+                    answer[j]++;
                 }
             }
-            answer = Math.max(answer, sum);
         }
-    
-   
-        return answer;
+        
+        // 가장 많이 선물 받을 친구의 선물 수 구하기.
+        return Arrays.stream(answer).max().orElse(-1);
     }
 }
