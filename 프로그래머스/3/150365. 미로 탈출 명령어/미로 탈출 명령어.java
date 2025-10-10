e0f910cd96aa6e81a72af16c134c2c66d5660aa3
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-10 19:37:12 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.49 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,46 +1,69 @@
 class Solution {
+    class Pos{
+        int x, y;
+        Pos(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    private StringBuilder sb = new StringBuilder(); 
+    private int[][] dir = {{1,0},{0,-1},{0,1},{-1,0}};
+    private char[] word = {'d', 'l', 'r', 'u'}; 
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        StringBuilder sb = new StringBuilder();
-        int moveCnt = 0;
-        int remain = distance(x,y,r,c);
-        if(k%2 != remain%2 || remain > k){
-            return "impossible";
-        }
- 
+  
+        // d, l, r, u, 
+        // 필요 이동 거리 k
+        // 현재 거리 10, = 남은 거리 10 이면  도착지로 이동해야한다.
+        // 현재 거리 4,  < 남은 거리 10 이면 사전순으로 이동한다.
+        // 현재 거리 10 > 남은 거리 4 이면 실패다.
+        Pos now = new Pos(x-1, y-1);
+        Pos end = new Pos(r-1, c-1);
         
-        // 1~n,m 범위
-        
-        // 남은 거리 + 이동한 횟수 <= k 이면 이동.
-        
-        // 남은 거리 + 이동한 횟수 > k(이동 해야할 거리) 이동을 못한다.
-        
-       
-        while(moveCnt < k){
-            // d, l, r, u 순서
-            if(isRange(n,m,x+1,y) && moveCnt+distance(x+1, y, r, c) <=k){
-                sb.append('d');
-                x = x+1;
-            }
-            else if(isRange(n,m,x,y-1) && moveCnt+distance(x, y-1, r, c) <=k){
-                    sb.append('l');
-                y -=1;
-            }else if(isRange(n,m,x,y+1) && moveCnt+distance(x, y+1, r, c) <=k){
-                    sb.append('r');
-                y +=1;
-            }else if(isRange(n,m,x-1,y) && moveCnt+distance(x-1, y, r, c) <=k){
-                    sb.append('u');
-                x = x-1;
-            }
-            moveCnt++;
+        for(int remainDist=k; remainDist>=0; remainDist--){
+            int dist = getDist(now, end); //현재 거리
+            if(dist > remainDist) return "impossible";
+            
+            if(dist == remainDist) goDestination(now, end, n, m);
+            
+            if(dist < remainDist) goDictionary(now, end, n, m);
+            
         }
         
-
         return sb.toString();
     }
-    public int distance(int x, int y, int r, int c){
-        return Math.abs(x-r) + Math.abs(y-c);
+    private void goDictionary(Pos now, Pos end, int n, int m){
+        for(int i=0; i<4; i++){
+            int nx = now.x + dir[i][0];
+            int ny = now.y + dir[i][1];
+
+            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+            
+            
+            now.x = nx;
+            now.y = ny;
+            sb.append(word[i]);  
+            break;
+        }
     }
-    public boolean isRange(int n, int m, int x, int y){
-        return x>=1 && y>=1 && x <=n && y <= m;
+    private void goDestination(Pos now, Pos end, int n, int m){
+        for(int i=0; i<4; i++){
+            int nx = now.x + dir[i][0];
+            int ny = now.y + dir[i][1];
+            
+            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+            
+            int nowDist = getDist(now, end);
+            int nextDist = getDist(new Pos(nx,ny), end);
+            
+            if(nowDist<=nextDist) continue;
+                
+            now.x = nx;
+            now.y = ny;
+            sb.append(word[i]);  
+            break;
+        }
+    }
+    private int getDist(Pos now, Pos end){
+        return Math.abs(now.x-end.x) + Math.abs(now.y-end.y);
     }
 }
