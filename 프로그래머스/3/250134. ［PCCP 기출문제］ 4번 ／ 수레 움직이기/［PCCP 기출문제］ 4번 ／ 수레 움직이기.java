1ea685dd35a69056420c25e6f90675331607684c
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-12 05:34:20 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 175.83 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -6,84 +6,92 @@ class Solution {
             this.y = y;
         }
     }
-    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-    private boolean[][][] visited;
-    private int n, m;
+    
     private int[][] maze;
+    private int n,m,answer;
+    private boolean[][][] visited;
     private boolean isRedEnd, isBlueEnd;
+    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    
     public int solution(int[][] maze) {
-
+        this.maze = maze;
+        
+        answer = 21;
         n = maze.length;
         m = maze[0].length;
-        visited = new boolean[n][m][2];
-        this.maze = maze;
+        
         Pos red = null;
         Pos blue = null;
+        visited = new boolean[n][m][2];
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<maze.length; i++){
+            for(int j=0; j<maze[0].length; j++){
                 if(maze[i][j]==1){
-                    red = new Pos(i,j);
                     visited[i][j][0] = true;
+                    red = new Pos(i,j);
                 }else if(maze[i][j]==2){
-                    blue = new Pos(i,j);
                     visited[i][j][1] = true;
+                    blue = new Pos(i,j);
                 }
             }
         }
-        int answer = dfs(red,blue,0);
-        return answer==21? 0:answer;
-    }
-    private int dfs(Pos red, Pos blue, int depth){
         
+        move(red, blue,0);
+        
+        return answer==21?0:answer;
+    }
+    
+    private void move(Pos red, Pos blue, int depth)
+    {
         if(isRedEnd && isBlueEnd){
-            return depth;
+        
+            answer = Math.min(answer, depth);
+            return;
         }
         
-        int result = 21;
-        
-        for(int i=0; i<4; i++){
-            Pos nextRed = isRedEnd? red : new Pos(red.x+dir[i][0], red.y+dir[i][1]);
-            for(int j=0; j<4; j++){
-                Pos nextBlue = isBlueEnd
-                    ? blue : new Pos(blue.x+dir[j][0], blue.y+dir[j][1]);
+        for(int i=0; i<4; i++)
+        {
+            Pos nextR = isRedEnd? red : new Pos(dir[i][0]+red.x, red.y + dir[i][1]);
+            for(int j=0; j<4; j++)
+            {
+                Pos nextB = isBlueEnd? blue : new Pos(dir[j][0]+blue.x, blue.y + dir[j][1]);
                 
-                if(!validate(red, blue, nextRed, nextBlue)) continue;
+                if(!validate(red, blue, nextR, nextB)) continue;
                 
-                if(maze[nextRed.x][nextRed.y]==3) isRedEnd = true;
-                if(maze[nextBlue.x][nextBlue.y]==4) isBlueEnd = true;
                 
-                visited[nextRed.x][nextRed.y][0] = true;
-                visited[nextBlue.x][nextBlue.y][1] = true;
+                visited[nextR.x][nextR.y][0] = true;
+                visited[nextB.x][nextB.y][1] = true;
                 
-                result = Math.min(result,dfs(nextRed, nextBlue, depth+1));
-                    
-                isRedEnd = false;
-                isBlueEnd = false;
+                if(maze[nextR.x][nextR.y]==3) isRedEnd = true;
+                if(maze[nextB.x][nextB.y]==4) isBlueEnd = true;
                 
-                visited[nextRed.x][nextRed.y][0] = false;
-                visited[nextBlue.x][nextBlue.y][1] = false;
+                move(nextR, nextB, depth+1);
+                
+                if(maze[nextR.x][nextR.y]==3) isRedEnd = false;
+                if(maze[nextB.x][nextB.y]==4) isBlueEnd = false;
+                visited[nextR.x][nextR.y][0] = false;
+                visited[nextB.x][nextB.y][1] = false;
             }
         }
         
-        return result;
     }
-    private boolean validate(Pos red, Pos blue, Pos nRed, Pos nBlue){
+    
+    private boolean validate(Pos prer, Pos preb, Pos r, Pos b)
+    {
+        // 1. 벽, 격자
+        if(r.x<0 || r.y<0 || b.x<0 || b.y<0 ||
+          r.x>=n || r.y>=m || b.x>=n || b.y>=m || 
+           maze[r.x][r.y] == 5 || maze[b.x][b.y]==5) return false;
         
-        if(nRed.x<0 || nRed.y<0 || nBlue.x<0 || nBlue.y<0 ||
-          nRed.x>=n || nRed.y>=m || nBlue.x>=n || nBlue.y>=m) return false;
-        
-        
-        if(maze[nRed.x][nRed.y]==5 || maze[nBlue.x][nBlue.y]==5) return false;
-        
-        if(!isRedEnd && visited[nRed.x][nRed.y][0]) return false;
-        if(!isBlueEnd && visited[nBlue.x][nBlue.y][1]) return false;
-        
-        if(nRed.x==nBlue.x && nRed.y==nBlue.y) return false;
-        
-        if(nRed.x==blue.x && nRed.y==blue.y &&
-          nBlue.x==red.x && nBlue.y==red.y) return false;
-        
+        // 2. 방문 칸
+        if(!isRedEnd && visited[r.x][r.y][0]) return false;
+        if(!isBlueEnd && visited[b.x][b.y][1]) return false;
+                
+        // 3. 동시 같은 칸
+        if(r.x==b.x && r.y==b.y) return false;
+        // 4. 자리 바꿈
+        if(r.x==preb.x && r.y==preb.y&& b.x==prer.x && b.y == prer.y) return false;
+
         return true;
     }
 }
