1be7e7bccf55f29d46e0299c982589501f44d5a3
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-12 04:31:32 +0900

    [level 3] Title: 홀짝트리, Time: 437.60 ms, Memory: 296 MB -BaekjoonHub

--- 프로그래머스/3/388354. 홀짝트리/홀짝트리.java
+++ 프로그래머스/3/388354. 홀짝트리/홀짝트리.java
@@ -1,74 +1,66 @@
 import java.util.*;
-class Visiter{
-    private int oddNode; // 홀수 -> 역홀
-    private int evenNode; // 짝수 -> 역짝
-    private int inverseOddNode; //역홀 -> 홀수
-    private int inverseEvenNode; // 역짝 -> 짝수
-
-    public void setNode(int root, int num){
-        int a = root%2;
-        int b = num%2;
-        if(a==0 && b==0){
-            evenNode++;
-        }
-        else if(a!=0 && b!=0){
-            oddNode++;
-        }
-        else if(a!=0 && b==0){
-            inverseOddNode++;
-        }
-        else inverseEvenNode++;
-    }
-
-    public int getOddEvenTreeCnt(){
-        if((oddNode==1 && evenNode==0) || (oddNode==0 && evenNode==1))
-            return 1;
-        return 0;
-    }
-
-    public int getInverseTreeCnt(){
-        if((inverseOddNode==1 && inverseEvenNode==0) || 
-           (inverseOddNode==0 && inverseEvenNode==1))
-            return 1;
-        return 0;
-    }
-}
 class Solution {
+    class Visitor {
+        int reverse;
+        int evenOdd;
+        
+        public void addTreeType(int curv, int edgeCnt)
+        {
+            if((curv+edgeCnt)%2==0) evenOdd++;
+            else reverse++;
+        }
+        
+        public void calcuAnswer()
+        {
+            if(evenOdd==1) answer[0]++;
+            if(reverse==1) answer[1]++;
+        }
+    }
+    
     private int[] answer = new int[2];
     private Set<Integer> visited = new HashSet();
-    private Map<Integer, List<Integer>> tree = new HashMap();
-
+    private Map<Integer, List<Integer>> treeMap = new HashMap();
+    
     public int[] solution(int[] nodes, int[][] edges) {
-
-        for(int node : nodes){
-            tree.put(node, new ArrayList());
+     
+        for(int[] edge : edges)
+        {
+            treeMap.computeIfAbsent(edge[0], (k) -> new ArrayList()).add(edge[1]);   
+            treeMap.computeIfAbsent(edge[1], (k) -> new ArrayList()).add(edge[0]);   
         }
-
-        for(int[] edge : edges){
-            tree.get(edge[0]).add(edge[1]);
-            tree.get(edge[1]).add(edge[0]);
-        }
-
-        for(int node : nodes){
+        
+        for(int node : nodes)
+        {
             if(visited.contains(node)) continue;
+         
+            if(treeMap.get(node)==null) treeMap.put(node, new ArrayList());
+            
             visited.add(node);
-            Visiter visiter = new Visiter();
-            dfs(node, visiter);
-            answer[0] += visiter.getOddEvenTreeCnt();
-            answer[1] += visiter.getInverseTreeCnt();
+            Visitor visitor = new Visitor();
+            
+            dfs(node, visitor);
+            
+            visitor.calcuAnswer();
         }
-
+        
         return answer;
     }
-    private void dfs(int now, Visiter visiter){
-        int cnt = 0;
-        for(int next : tree.get(now)){
-            cnt++;
-            if(visited.contains(next)) continue;
-            visited.add(next);
-            dfs(next, visiter);
+    
+    private void dfs(int curv, Visitor visitor)
+    {
+        
+        int edgeCnt = 0;
+        
+        for(int nextv : treeMap.get(curv))
+        {
+            edgeCnt++;
+            
+            if(visited.contains(nextv)) continue;
+            
+            visited.add(nextv);
+            dfs(nextv, visitor);
         }
-
-        visiter.setNode(now, cnt);
+        
+        visitor.addTreeType(curv, edgeCnt);
     }
 }
