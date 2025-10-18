fb0c49f126899d91a4f5a19e3f74df115a5c2cc9
jihaneol <104291422+jihaneol@users.noreply.github.com>
2025-10-18 23:31:43 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.74 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,69 +1,44 @@
+import java.util.*;
 class Solution {
-    class Pos{
-        int x, y;
-        Pos(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    private StringBuilder sb = new StringBuilder(); 
-    private int[][] dir = {{1,0},{0,-1},{0,1},{-1,0}};
-    private char[] word = {'d', 'l', 'r', 'u'}; 
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-  
-        // d, l, r, u, 
-        // 필요 이동 거리 k
-        // 현재 거리 10, = 남은 거리 10 이면  도착지로 이동해야한다.
-        // 현재 거리 4,  < 남은 거리 10 이면 사전순으로 이동한다.
-        // 현재 거리 10 > 남은 거리 4 이면 실패다.
-        Pos now = new Pos(x-1, y-1);
-        Pos end = new Pos(r-1, c-1);
+        StringBuilder sb = new StringBuilder();
+        int dist = getDist(x,y,r,c);
+        if(dist>k || (k-dist)%2!=0) return "impossible";
         
-        for(int remainDist=k; remainDist>=0; remainDist--){
-            int dist = getDist(now, end); //현재 거리
-            if(dist > remainDist) return "impossible";
+        while(k-->0){
             
-            if(dist == remainDist) goDestination(now, end, n, m);
+            int ux = x - 1;
+            int dx = x + 1;
+            int uy = y + 1;
+            int dy = y - 1;
             
-            if(dist < remainDist) goDictionary(now, end, n, m);
+            // d
+            if(dx <= n && k>=getDist(dx, y, r, c)){
+                sb.append('d');
+                x = dx;
+            }
+            // l
+            else if(dy>=1 && k>=getDist(x,dy,r,c)){
+                sb.append('l');
+                y = dy;
+            }
+            // r 
+            else if(uy <= m && k>=getDist(x,uy,r,c)){
+                sb.append('r');
+                y = uy;
+            }
+            // u
+            else{
+                sb.append('u');
+                x = ux;
+            }
             
         }
         
         return sb.toString();
     }
-    private void goDictionary(Pos now, Pos end, int n, int m){
-        for(int i=0; i<4; i++){
-            int nx = now.x + dir[i][0];
-            int ny = now.y + dir[i][1];
-
-            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-            
-            
-            now.x = nx;
-            now.y = ny;
-            sb.append(word[i]);  
-            break;
-        }
-    }
-    private void goDestination(Pos now, Pos end, int n, int m){
-        for(int i=0; i<4; i++){
-            int nx = now.x + dir[i][0];
-            int ny = now.y + dir[i][1];
-            
-            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-            
-            int nowDist = getDist(now, end);
-            int nextDist = getDist(new Pos(nx,ny), end);
-            
-            if(nowDist<=nextDist) continue;
-                
-            now.x = nx;
-            now.y = ny;
-            sb.append(word[i]);  
-            break;
-        }
-    }
-    private int getDist(Pos now, Pos end){
-        return Math.abs(now.x-end.x) + Math.abs(now.y-end.y);
+    
+    private int getDist(int x, int y, int r, int c){
+        return Math.abs(x-r) + Math.abs(y-c);
     }
 }
